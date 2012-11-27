7632011f74598fcbdf43365461589d5161583c48
Bohuslav Kabrda <bkabrda@redhat.com>
2012-11-27 13:22:49 +0100

    Get rid of the OpenShift stuff, rename the containing dir to coprs_frontend, include sample apache config file

--- coprs_frontend/coprs/config.py
+++ coprs_frontend/coprs/config.py
@@ -1,16 +1,9 @@
 import os
 
-REPO_DIR = ''
-if 'OPENSHIFT_REPO_DIR' in os.environ:
-    REPO_DIR = os.environ['OPENSHIFT_REPO_DIR']
-
-DATA_DIR = ''
-if 'OPENSHIFT_DATA_DIR' in os.environ:
-    DATA_DIR = os.environ['OPENSHIFT_DATA_DIR']
-
 class Config(object):
-    DATABASE = os.path.join(REPO_DIR, '../data/copr.db')
-    OPENID_STORE = os.path.join(DATA_DIR, '../openid_store')
+    DATA_DIR = os.path.join(os.path.dirname(__file__), '../../data')
+    DATABASE = os.path.join(DATA_DIR, 'copr.db')
+    OPENID_STORE = os.path.join(DATA_DIR, 'openid_store')
     SECRET_KEY = 'THISISNOTASECRETATALL'
     BACKEND_PASSWORD = 'thisisbackend'
 
@@ -18,7 +11,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.abspath(DATABASE)
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
