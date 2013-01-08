e6d24e582bea3d0e12d70500742a9772c99b6894
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-01-08 11:41:31 +0100

    Add alembic revision to update the database scheme

---
+++ coprs_frontend/alembic/versions/32ba137a3d56_add_token_informatio.py
@@ -0,0 +1,33 @@
+"""Add token information to the user table
+
+Revision ID: 32ba137a3d56
+Revises: 595a31c145fb
+Create Date: 2013-01-07 20:56:14.698735
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '32ba137a3d56'
+down_revision = '595a31c145fb'
+
+from alembic import op
+import sqlalchemy as sa
+
+import datetime
+
+
+def upgrade():
+    """ Add the coluns api_token and api_token_expiration to the user table.
+    """
+    op.add_column('user', sa.Column('api_token', sa.String(40),
+        nullable=False), default='default_token')
+    op.add_column('user', sa.Column('api_token_expiration', sa.Date,
+        nullable=False, default=datetime.date(2000, 1, 1)))
+
+
+def downgrade():
+    """ Drop the coluns api_token and api_token_expiration to the user table.
+    """
+    op.drop_column('user', 'api_token')
+    op.drop_column('user', 'api_token_expiration')
+
