89f33ea9d6e768c7e40394394ae2507274131e30
Seth Vidal <skvidal@fedoraproject.org>
2013-01-14 12:55:57 -0500

    - add the job base repo to the list of repos to include in mock - for later builds - record the build id when finished so we can match them up

--- backend/dispatcher.py
+++ backend/dispatcher.py
@@ -163,6 +163,8 @@ class Worker(multiprocessing.Process):
         jobdata.destdir = self.opts.destdir + '/' + build['copr']['owner']['name'] + '/' + build['copr']['name'] + '/'
         jobdata.build_id = build['id']
         jobdata.results = self.opts.results_baseurl + '/' + build['copr']['owner']['name'] + '/' + build['copr']['name'] + '/'
+        # add the results dir to the jobdata.repos
+        jobdata.repos.append(jobdata.results)
         jobdata.copr_id = build['copr']['id']
         jobdata.user_id = build['user_id']
         return jobdata
@@ -293,7 +295,7 @@ class Worker(multiprocessing.Process):
                     # check if any pkgs didn't build
                     if mr.failed: 
                         status = 0
-                self.callback.log('Finished build: builder=%r timeout=%r destdir=%r chroot=%r repos=%r' % (ip, job.timeout, job.destdir, chroot, str(job.repos)))
+                self.callback.log('Finished build: id=%r builder=%r timeout=%r destdir=%r chroot=%r repos=%r' % (job.build_id, ip, job.timeout, job.destdir, chroot, str(job.repos)))
             
             job.ended_on = time.time()
             
