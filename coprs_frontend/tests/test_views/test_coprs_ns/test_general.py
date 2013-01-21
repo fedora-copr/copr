23a9df82689bafb10af6231ec2ae636ca19b6025
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-21 12:11:56 +0100

    Fix the rest of the tests failing after the UI remake

--- coprs_frontend/tests/test_views/test_coprs_ns/test_general.py
+++ coprs_frontend/tests/test_views/test_coprs_ns/test_general.py
@@ -4,11 +4,11 @@ from tests.coprs_test_case import CoprsTestCase
 
 class TestCoprsShow(CoprsTestCase):
     def test_show_no_entries(self):
-        assert 'No entries' in self.tc.get('/').data
+        assert 'No coprs...' in self.tc.get('/').data
 
     def test_show_more_entries(self, f_users, f_coprs):
         r = self.tc.get('/')
-        assert r.data.count('<div class=copr>') == 3
+        assert r.data.count('<div class="copr">') == 3
 
 class TestCoprsOwned(CoprsTestCase):
     def test_owned_none(self, f_users, f_coprs):
@@ -18,7 +18,7 @@ class TestCoprsOwned(CoprsTestCase):
 
             self.db.session.add(self.u3)
             r = c.get('/coprs/owned/{0}/'.format(self.u3.name))
-            assert 'No entries' in r.data
+            assert 'No coprs...' in r.data
 
     def test_owned_one(self, f_users, f_coprs):
         with self.tc as c:
@@ -27,7 +27,7 @@ class TestCoprsOwned(CoprsTestCase):
 
             self.db.session.add(self.u1)
             r = c.get('/coprs/owned/{0}/'.format(self.u1.name))
-            assert r.data.count('<div class=copr>') == 1
+            assert r.data.count('<div class="copr">') == 1
 
 class TestCoprsAllowed(CoprsTestCase):
     def test_allowed_none(self, f_users, f_coprs, f_copr_permissions):
@@ -37,7 +37,7 @@ class TestCoprsAllowed(CoprsTestCase):
 
             self.db.session.add(self.u3)
             r = c.get('/coprs/allowed/{0}/'.format(self.u3.name))
-            assert 'No entries' in r.data
+            assert 'No coprs...' in r.data
 
     def test_allowed_one(self, f_users, f_coprs, f_copr_permissions):
         with self.tc as c:
@@ -46,7 +46,7 @@ class TestCoprsAllowed(CoprsTestCase):
 
             self.db.session.add(self.u1)
             r = c.get('/coprs/allowed/{0}/'.format(self.u1.name))
-            assert r.data.count('<div class=copr>') == 1
+            assert r.data.count('<div class="copr">') == 1
 
     def test_allowed_one_but_asked_for_one_more(self, f_users, f_coprs, f_copr_permissions):
         with self.tc as c:
@@ -55,7 +55,7 @@ class TestCoprsAllowed(CoprsTestCase):
 
             self.db.session.add(self.u1)
             r = c.get('/coprs/allowed/{0}/'.format(self.u1.name))
-            assert r.data.count('<div class=copr>') == 1
+            assert r.data.count('<div class="copr">') == 1
 
 class TestCoprNew(CoprsTestCase):
     success_string = 'New copr was successfully created'
@@ -126,16 +126,18 @@ class TestCoprDetail(CoprsTestCase):
         assert self.c1.name in r.data
 
     def test_copr_detail_contains_builds(self, f_users, f_coprs, f_builds):
-        r = self.tc.get('/coprs/detail/{0}/{1}/'.format(self.u1.name, self.c1.name))
-        assert r.data.count('<tr class=build') == 2
+        r = self.tc.get('/coprs/detail/{0}/{1}/builds/'.format(self.u1.name, self.c1.name))
+        print r.data
+        assert r.data.count('<tr class="build') == 2
 
     def test_copr_detail_anonymous_doesnt_contain_permissions_table_when_no_permissions(self, f_users, f_coprs, f_copr_permissions):
-        r = self.tc.get('/coprs/detail/{0}/{1}/'.format(self.u1.name, self.c1.name))
-        assert '<table class=permissions' not in r.data
+        r = self.tc.get('/coprs/detail/{0}/{1}/permissions/'.format(self.u1.name, self.c1.name))
+        assert '<table class="permissions"' not in r.data
 
     def test_copr_detail_contains_permissions_table(self, f_users, f_coprs, f_copr_permissions):
-        r = self.tc.get('/coprs/detail/{0}/{1}/'.format(self.u2.name, self.c3.name))
-        assert '<table class=permissions' in r.data
+        r = self.tc.get('/coprs/detail/{0}/{1}/permissions/'.format(self.u2.name, self.c3.name))
+        print r.data
+        assert '<table class="permissions-table"' in r.data
         assert '<td>{0}'.format(self.u3.name) in r.data
         assert '<td>{0}'.format(self.u1.name) in r.data
 
@@ -149,7 +151,7 @@ class TestCoprDetail(CoprsTestCase):
                 s['openid'] = self.u1.openid_name
 
             self.db.session.add_all([self.u2, self.c2])
-            r = c.get('/coprs/detail/{0}/{1}/'.format(self.u2.name, self.c2.name))
+            r = c.get('/coprs/detail/{0}/{1}/permissions/'.format(self.u2.name, self.c2.name))
             # u1 is approved builder, check for that
             assert '/permissions_applier_change/' in r.data
 
@@ -159,7 +161,7 @@ class TestCoprDetail(CoprsTestCase):
                 s['openid'] = self.u2.openid_name
 
             self.db.session.add_all([self.u2, self.c2])
-            r = c.get('/coprs/detail/{0}/{1}/'.format(self.u2.name, self.c2.name))
+            r = c.get('/coprs/detail/{0}/{1}/permissions/'.format(self.u2.name, self.c2.name))
             assert '/permissions_applier_change/' not in r.data
 
     def test_detail_has_correct_permissions_form(self, f_users, f_coprs, f_copr_permissions):
@@ -168,7 +170,7 @@ class TestCoprDetail(CoprsTestCase):
                 s['openid'] = self.u2.openid_name
 
             self.db.session.add_all([self.u2, self.c3])
-            r = c.get('/coprs/detail/{0}/{1}/'.format(self.u2.name, self.c3.name))
+            r = c.get('/coprs/detail/{0}/{1}/permissions/'.format(self.u2.name, self.c3.name))
 
             assert r.data.count('nothing') == 2
             assert '<select id="copr_builder_1" name="copr_builder_1">' in r.data
@@ -184,7 +186,7 @@ class TestCoprDetail(CoprsTestCase):
                 s['openid'] = self.u1.openid_name
 
             self.db.session.add_all([self.u2, self.c2])
-            r = c.get('/coprs/detail/{0}/{1}/'.format(self.u2.name, self.c2.name))
+            r = c.get('/coprs/detail/{0}/{1}/builds/'.format(self.u2.name, self.c2.name))
             assert '/cancel_build/' not in r.data
 
     def test_copr_detail_allows_submitter_to_cancel_build(self, f_users, f_coprs, f_builds):
@@ -193,7 +195,7 @@ class TestCoprDetail(CoprsTestCase):
                 s['openid'] = self.u2.openid_name
 
             self.db.session.add_all([self.u2, self.c2])
-            r = c.get('/coprs/detail/{0}/{1}/'.format(self.u2.name, self.c2.name))
+            r = c.get('/coprs/detail/{0}/{1}/builds/'.format(self.u2.name, self.c2.name))
             assert '/cancel_build/' in r.data
 
 
@@ -243,11 +245,15 @@ class TestCoprUpdate(CoprsTestCase):
             r = c.post('/coprs/detail/{0}/{1}/update/'.format(self.u1.name, self.c1.name),
                        data = {'name': self.c1.name, self.mc2.chroot_name: 'y', self.mc3.chroot_name: 'y', 'id': self.c1.id},
                        follow_redirects = True)
-            self.db.session.add_all([self.mc1, self.mc2, self.mc3])
             assert 'Copr was updated successfully' in r.data
-            assert self.mc2.chroot_name in r.data
-            assert self.mc3.chroot_name in r.data
-            assert self.mc1.chroot_name not in r.data
+            self.db.session.add_all([self.c1, self.mc1, self.mc2, self.mc3])
+            mock_chroots = self.models.MockChroot.query.join(self.models.CoprChroot).\
+                                                        filter(self.models.CoprChroot.copr_id==\
+                                                               self.c1.id).all()
+            mock_chroots_names = map(lambda x: x.chroot_name, mock_chroots)
+            assert self.mc2.chroot_name in mock_chroots_names
+            assert self.mc3.chroot_name in mock_chroots_names
+            assert self.mc1.chroot_name not in mock_chroots_names
 
 
 class TestCoprApplyForPermissions(CoprsTestCase):
