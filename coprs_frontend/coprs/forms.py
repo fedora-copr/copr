c7e23bd75c6a36067d8e5f93b9751e3318eaacbf
Richard Marko <rmarko@fedoraproject.org>
2013-10-29 14:34:17 +0100

    PEP8 fixes
    Signed-off-by: Richard Marko <rmarko@fedoraproject.org>

--- coprs_frontend/coprs/forms.py
+++ coprs_frontend/coprs/forms.py
@@ -11,10 +11,12 @@ from coprs import helpers
 from coprs import models
 from coprs.logic import coprs_logic
 
+
 class UrlListValidator(object):
-    def __init__(self, message = None):
+    def __init__(self, message=None):
         if not message:
-            message = 'A list of URLs separated by whitespace characters is needed ("{0}" doesn\'t seem to be a URL).'
+            message = 'A list of URLs separated by whitespace characters'
+            ' is needed ("{0}" doesn\'t seem to be a URL).'
         self.message = message
 
     def __call__(self, form, field):
@@ -36,13 +38,14 @@ class UrlListValidator(object):
 
 
 class CoprUniqueNameValidator(object):
-    def __init__(self, message = None):
+    def __init__(self, message=None):
         if not message:
             message = 'You already have project named "{0}".'
         self.message = message
 
     def __call__(self, form, field):
-        existing = coprs_logic.CoprsLogic.exists_for_user(flask.g.user, field.data).first()
+        existing = coprs_logic.CoprsLogic.exists_for_user(
+            flask.g.user, field.data).first()
 
         if existing and str(existing.id) != form.id.data:
             raise wtforms.ValidationError(self.message.format(field.data))
@@ -59,18 +62,21 @@ class StringListFilter(object):
         regex = re.compile(r'\s+')
         return regex.sub(lambda x: '\n', result)
 
+
 class ValueToPermissionNumberFilter(object):
     def __call__(self, value):
         if value:
             return helpers.PermissionEnum('request')
         return helpers.PermissionEnum('nothing')
 
+
 class CoprFormFactory(object):
     @staticmethod
     def create_form_cls(mock_chroots=None):
         class F(wtf.Form):
-            # also use id here, to be able to find out whether user is updating a copr
-            # if so, we don't want to shout that name already exists
+            # also use id here, to be able to find out whether user
+            # is updating a copr if so, we don't want to shout
+            # that name already exists
             id = wtforms.HiddenField()
 
             name = wtforms.TextField(
@@ -111,7 +117,8 @@ class CoprFormFactory(object):
                     return False
 
                 if not self.validate_mock_chroots_not_empty():
-                    self._mock_chroots_error = 'At least one chroot must be selected'
+                    self._mock_chroots_error = 'At least one chroot' \
+                        'must be selected'
                     return False
                 return True
 
@@ -122,13 +129,19 @@ class CoprFormFactory(object):
                         have_any = True
                 return have_any
 
-        F.chroots_list = map(lambda x: x.chroot_name, models.MockChroot.query.filter(models.MockChroot.is_active==True).all())
+        F.chroots_list = map(lambda x: x.chroot_name,
+                             models.MockChroot.query.filter(
+                                 models.MockChroot.is_active == True
+                             ).all())
         F.chroots_list.sort()
-        F.chroots_sets = {} # sets of chroots according to how we should print them in columns
+        # sets of chroots according to how we should print them in columns
+        F.chroots_sets = {}
         for ch in F.chroots_list:
             checkbox_default = False
-            if mock_chroots and ch in map(lambda x:x.chroot_name, mock_chroots):
+            if mock_chroots and ch in map(lambda x: x.chroot_name,
+                                          mock_chroots):
                 checkbox_default = True
+
             setattr(F, ch, wtforms.BooleanField(ch, default=checkbox_default))
             if ch[0] in F.chroots_sets:
                 F.chroots_sets[ch[0]].append(ch)
@@ -136,6 +149,7 @@ class CoprFormFactory(object):
                 F.chroots_sets[ch[0]] = [ch]
 
         return F
+
 
 class CoprDeleteForm(wtf.Form):
     verify = wtforms.TextField(
@@ -146,6 +160,7 @@ class CoprDeleteForm(wtf.Form):
                 r'^yes$',
                 message='Type "yes" - without the quotes, lowercase.')
         ])
+
 
 class BuildForm(wtf.Form):
     pkgs = wtforms.TextAreaField(
@@ -171,12 +186,14 @@ class BuildForm(wtf.Form):
                 max=constants.MAX_BUILD_TIMEOUT)],
         default=constants.DEFAULT_BUILD_TIMEOUT)
 
+
 class CoprLegalFlagForm(wtf.Form):
     comment = wtforms.TextAreaField('Comment')
 
+
 class PermissionsApplierFormFactory(object):
     @staticmethod
-    def create_form_cls(permission = None):
+    def create_form_cls(permission=None):
         class F(wtf.Form):
             pass
 
