1fa19ba328445e9c5df9c0e52d5ed8a090821bd2
Miroslav Suchý <miroslav@suchy.cz>
2014-01-14 16:44:26 +0100

    [frontend] do not traceback when user malform url
    addressing:
    Exception on /coprs/kevin/calibre/repo/f/ [GET]
    Traceback (most recent call last):
      File "/usr/lib/python2.7/site-packages/flask/app.py", line 1687, in wsgi_app
        response = self.full_dispatch_request()
      File "/usr/lib/python2.7/site-packages/flask/app.py", line 1360, in full_dispatch_request
        rv = self.handle_user_exception(e)
      File "/usr/lib/python2.7/site-packages/flask/app.py", line 1358, in full_dispatch_request
        rv = self.dispatch_request()
      File "/usr/lib/python2.7/site-packages/flask/app.py", line 1344, in dispatch_request
        return self.view_functions[rule.endpoint](**req.view_args)
      File "/usr/share/copr/coprs_frontend/coprs/views/coprs_ns/coprs_general.py", line 332, in generate_repo_file
        mock_chroot = coprs_logic.MockChrootsLogic.get_from_name(chroot).one()
      File "/usr/share/copr/coprs_frontend/coprs/logic/coprs_logic.py", line 297, in get_from_name
        name_tuple = cls.tuple_from_name(None, chroot_name)
      File "/usr/share/copr/coprs_frontend/coprs/logic/coprs_logic.py", line 357, in tuple_from_name
        'Chroot Name doesn\'t contain dash, can\'t determine chroot architecure.')
    MalformedArgumentException: Chroot Name doesn't contain dash, can't determine chroot architecure.

--- coprs_frontend/coprs/views/coprs_ns/coprs_general.py
+++ coprs_frontend/coprs/views/coprs_ns/coprs_general.py
@@ -330,6 +330,8 @@ def generate_repo_file(username, coprname, chroot):
                 with_builds=True).one()
     except sqlalchemy.orm.exc.NoResultFound:
         return page_not_found('Project {0}/{1} does not exist'.format(username, coprname))
+    except exceptions.MalformedArgumentException, e:
+        return page_not_found("%s" % e)
 
     try:
         mock_chroot = coprs_logic.MockChrootsLogic.get_from_name(chroot).one()
