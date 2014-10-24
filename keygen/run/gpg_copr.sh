9d71f2d1c453ee621a2952d42b1bbe6a66b8f5a0
Valentin Gologuzov <vgologuz@redhat.com>
2014-10-24 14:49:54 +0200

    [keygen] added option "--no-auto-check-trustdb" to gpg wrapper. should fix long time to access large keyring after any change.

--- keygen/run/gpg_copr.sh
+++ keygen/run/gpg_copr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-runuser -u copr-signer  /usr/bin/gpg2 -- --homedir /var/lib/copr-keygen/gnupg  $@
+runuser -u copr-signer  /usr/bin/gpg2 -- --homedir /var/lib/copr-keygen/gnupg --no-auto-check-trustdb  $@
