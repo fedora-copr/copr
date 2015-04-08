3090376c0d41f91ba4ecc8e4aad5eb92d46408f7
Valentin Gologuzov <vgologuz@redhat.com>
2015-04-08 14:12:21 +0200

    [backend] updated builder playbooks

--- backend/conf/playbooks/filter_plugins/os_nova.py
+++ backend/conf/playbooks/filter_plugins/os_nova.py
@@ -1,13 +1,23 @@
 from novaclient.v1_1.client import Client
 
+
 def nova_result_to_builder_ip(nova_result, network_name):
     return nova_result["addresses"][network_name][0]["addr"]
 
+
 def network_name_to_id(network_name, username, password, tenant_name, auth_url):
-    nt = Client(username, password, tenant_name, auth_url, insecure=True)
-    # import ipdb; ipdb.set_trace()
+    nt = Client(username, password, tenant_name, auth_url)
     return nt.networks.find(label=network_name).id
-       
+
+
+def image_name_to_id(image_name, username, password, tenant_name, auth_url):
+    nt = Client(username, password, tenant_name, auth_url)
+    return nt.images.find(name=image_name).id
+
+
+def flavor_name_to_id(flavor_name, username, password, tenant_name, auth_url):
+    nt = Client(username, password, tenant_name, auth_url)
+    return nt.flavors.find(name=flavor_name).id
 
 
 class FilterModule(object):
@@ -15,9 +25,9 @@ class FilterModule(object):
         return {
             "nova_result_to_builder_ip": nova_result_to_builder_ip,
             # "flavor_id_to_name": flavor_id_to_name,
-            # "flavor_name_to_id": flavor_name_to_id,
+            "flavor_name_to_id": flavor_name_to_id,
             # "image_id_to_name": image_id_to_name,
-            # "image_name_to_id": image_name_to_id,
+            "image_name_to_id": image_name_to_id,
             "network_name_to_id": network_name_to_id,
             # "network_id_to_name": network_id_to_name,
         }
