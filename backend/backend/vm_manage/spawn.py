8b241d162b01520c1cd480cf21efd2740eb738e7
Miroslav Suchý <msuchy@redhat.com>
2015-06-11 11:12:03 +0200

    \n is repr() so it is literaly there

--- backend/backend/vm_manage/spawn.py
+++ backend/backend/vm_manage/spawn.py
@@ -43,12 +43,12 @@ def spawn_instance(spawn_playbook, log):
 
     if not result:
         raise CoprSpawnFailError("No result, trying again")
-    match = re.search(r'IP=([^\{\}"\n]+)', result, re.MULTILINE)
+    match = re.search(r'IP=([^\{\}"\n\\]+)', result, re.MULTILINE)
 
     if not match:
         raise CoprSpawnFailError("No ip in the result, trying again")
     ipaddr = match.group(1)
-    match = re.search(r'vm_name=([^\{\}"\n]+)', result, re.MULTILINE)
+    match = re.search(r'vm_name=([^\{\}"\n\\]+)', result, re.MULTILINE)
 
     if match:
         vm_name = match.group(1)
