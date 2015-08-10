4c8a7cfe59132cec6b5ebc5a3de247d87641c655
Jakub Kadlčík <jkadlcik@redhat.com>
2015-08-10 15:29:06 +0200

    [frontend] add experimental support for repo RPM packages (RhBug:1227696)

---
+++ frontend/coprs_frontend/coprs/templates/coprs/copr-repo-package.spec
@@ -0,0 +1,33 @@
+Name:           %{pkg_name}
+Version:        %{pkg_version}
+Release:        %{pkg_release}
+Summary:        COPR %{user}/%{copr} Repository Configuration
+
+Group:          System Environment/Base
+License:        GPLv2+
+URL:            https://copr.fedoraproject.org/coprs/%{user}/%{copr}/
+Source0:        https://copr.fedoraproject.org/coprs/frostyx/tracer/repo/%{chroot}/%{user}-%{copr}-%{chroot}.repo
+BuildArch:      noarch
+
+
+%description
+This is configuration package for COPR repository %{user}/%{copr}
+
+%prep
+echo "Nothing to prep"
+
+%build
+echo "Nothing to build"
+
+%install
+mkdir -p %{buildroot}%{_sysconfdir}/yum.repos.d
+%{__install} -p -m644 %{SOURCE0} \
+    $RPM_BUILD_ROOT%{_sysconfdir}/yum.repos.d/%{repofile}
+
+%files
+%config(noreplace) %{_sysconfdir}/yum.repos.d/%{repofile}
+
+
+%changelog
+# @TODO
+# - Initial package
---
+++ frontend/coprs_frontend/run/generate-repo-packages.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# RUN
+#     cd frontend/coprs_frontend
+#     COPR_CONFIG=../config/copr_devel.conf python run/generate-repo-packages.py
+
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+
+import requests
+import urlparse
+import os
+import shutil
+import subprocess
+
+here = os.path.dirname(os.path.realpath(__file__))
+import sys
+sys.path.append(os.path.dirname(here))
+
+from coprs import app
+from coprs.logic.coprs_logic import CoprsLogic
+from copr.client.exceptions import CoprException, CoprRequestException
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+FRONTEND_URL = app.config["PUBLIC_COPR_HOSTNAME"]
+FRONTEND_URL = "http://127.0.0.1:5000/"
+FRONTEND_URL = "http://copr.fedoraproject.org/"
+FRONTEND_URL = "http://copr-fe-dev.cloud.fedoraproject.org/"
+
+PACKAGES_DIR = os.path.join(app.config["DATA_DIR"], "repo-rpm-packages")
+PACKAGES_DIR = "/tmp/repo-rpm-packages"  # @TODO Use frontend's directory containing structure of package's builds
+
+RPMBUILD = os.path.join(os.path.expanduser("~"), "rpmbuild")
+RPMBUILD = "/tmp/rpmbuild"
+
+VERSION = 1
+RELEASE = 1
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class RepoRpmBuilder(object):
+
+    RPM_NAME_FORMAT = "copr-repo-{}-{}-{}-{}-1-1.noarch.rpm"
+    SPEC_NAME = "copr-repo-package.spec"
+
+    def __init__(self, user, copr, chroot, topdir=RPMBUILD, packagesdir=PACKAGES_DIR):
+        self.user = user                # Name of the user
+        self.copr = copr                # Name of the copr
+        self.chroot = chroot            # MockChroot object
+        self.topdir = topdir            # rpmbuild directory (default $HOME/rpmbuild)
+        self.packagesdir = packagesdir  # Directory where to store the rpm packages
+
+    @property
+    def rpm_name(self):
+        return self.RPM_NAME_FORMAT.format(self.user, self.copr, self.chroot.os_release, self.chroot.os_version)
+
+    @property
+    def repo_name(self):
+        return "{}-{}-{}-{}.repo"\
+            .format(self.user, self.copr, self.chroot.os_release, self.chroot.os_version)
+
+    def has_repo_package(self):
+        return os.path.isfile(os.path.join(self.packagesdir, self.rpm_name))
+
+    def get_repofile(self):
+        api = "coprs/{}/{}/repo/{}".format(self.user, self.copr, self.chroot.name)
+        url = urlparse.urljoin(FRONTEND_URL, api)
+        r = requests.get(url)
+        if r.status_code != 200:
+            raise CoprRequestException("Can't get {}".format(url))
+        return r.content
+
+    def generate_repo_package(self):
+
+        shutil.copyfile(os.path.join("coprs/templates/coprs/", self.SPEC_NAME),
+                        os.path.join(self.topdir, "SPECS", self.SPEC_NAME))
+
+        with open(os.path.join(self.topdir, "SOURCES", self.repo_name), "w") as f:
+            f.writelines(self.get_repofile())
+
+        defines = [
+            "-D",       "_topdir {}".format(self.topdir),
+            "-D",       "_rpmdir {}".format(self.packagesdir),
+            "-D",  "_rpmfilename {}".format(self.rpm_name),
+            "-D",      "pkg_name {}".format("copr-repo-{}-{}".format(self.user, self.copr)),
+            "-D",   "pkg_version {}".format(VERSION),
+            "-D",   "pkg_release {}".format(RELEASE),
+            "-D",          "user {}".format(self.user),
+            "-D",          "copr {}".format(self.copr),
+            "-D",        "chroot {}".format("{}-{}".format(self.chroot.os_release, self.chroot.os_version)),
+            "-D",      "repofile {}".format("_copr_{}-{}.repo".format(self.user, self.copr)),
+        ]
+
+        command = ["rpmbuild", "-ba"] + defines + [os.path.join(self.topdir, "SPECS", self.SPEC_NAME)]
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+
+        if process.returncode != 0:
+            raise CoprException("Failed rpmbuild for: {}\n{}".format(self.repo_name, err))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def all_coprs():
+    return CoprsLogic.get_all()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def prepare_rpmbuild_directory():
+    dirs = ["BUILD", "RPMS", "SOURCES", "SPECS", "SRPMS"]
+    for d in dirs:
+        d = os.path.join(RPMBUILD, d)
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+
+def prepare_packages_directory():
+    if not os.path.exists(PACKAGES_DIR):
+        os.makedirs(PACKAGES_DIR)
+
+
+def unique_chroots(copr):
+    d = {}
+    for chroot in copr.active_chroots:
+        d[chroot.name_release] = chroot
+    return d.values()
+
+
+def main():
+    prepare_rpmbuild_directory()
+    prepare_packages_directory()
+
+    for copr in all_coprs():
+        for chroot in unique_chroots(copr):
+            builder = RepoRpmBuilder(user=copr.owner.name, copr=copr.name, chroot=chroot)
+
+            if builder.has_repo_package():
+                print("Skipping {}".format(builder.repo_name))
+                continue
+
+            try:
+                builder.generate_repo_package()
+                print("Created RPM package for: {}".format(builder.repo_name))
+
+            except CoprException as e:
+                print(e.message, file=sys.stderr)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
