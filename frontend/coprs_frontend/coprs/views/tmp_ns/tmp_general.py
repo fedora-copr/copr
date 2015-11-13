98ec2c226ae461630d80421e4ce5141e278cad22
Miroslav Suchý <msuchy@redhat.com>
2015-11-13 13:47:59 +0100

    W:  7,25: Redefining built-in file (redefined-builtin)

--- frontend/coprs_frontend/coprs/views/tmp_ns/tmp_general.py
+++ frontend/coprs_frontend/coprs/views/tmp_ns/tmp_general.py
@@ -4,6 +4,6 @@ from coprs.views.tmp_ns import tmp_ns
 from coprs import app
 
 @tmp_ns.route("/<directory>/<file>")
-def give_srpm(directory, file):
+def give_srpm(directory, file_path):
     path = os.path.join(app.config["SRPM_STORAGE_DIR"], directory)
-    return flask.send_from_directory(path, file)
+    return flask.send_from_directory(path, file_path)
