0b900e1dbf5782c568f7dc88021db8ba57336d43
Miroslav Suchý <msuchy@redhat.com>
2015-11-18 10:41:59 +0100

    use the same variable as defined in route

--- frontend/coprs_frontend/coprs/views/tmp_ns/tmp_general.py
+++ frontend/coprs_frontend/coprs/views/tmp_ns/tmp_general.py
@@ -3,7 +3,7 @@ import os
 from coprs.views.tmp_ns import tmp_ns
 from coprs import app
 
-@tmp_ns.route("/<directory>/<file>")
+@tmp_ns.route("/<directory>/<file_path>")
 def give_srpm(directory, file_path):
     path = os.path.join(app.config["SRPM_STORAGE_DIR"], directory)
     return flask.send_from_directory(path, file_path)
