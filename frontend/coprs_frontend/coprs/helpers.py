1d49db6ac4be993ce7c0e6621758b313b8026ec8
clime <clime@redhat.com>
2016-01-04 16:02:37 +0100

    [frontend] dependency on dnf package removed by providing own impl of SplitFilename function in coprs/helpers.py

--- frontend/coprs_frontend/coprs/helpers.py
+++ frontend/coprs_frontend/coprs/helpers.py
@@ -11,7 +11,6 @@ from dateutil import parser as dt_parser
 from netaddr import IPAddress, IPNetwork
 from redis import StrictRedis
 from sqlalchemy.types import TypeDecorator, VARCHAR
-from dnf.rpm.miscutils import splitFilename
 import json
 
 from coprs import constants
@@ -219,6 +218,37 @@ def branch_to_os_version(branch):
         os = "epel"
         version = branch[-1:]
     return os, version
+
+
+def splitFilename(filename):
+    """
+    Pass in a standard style rpm fullname
+
+    Return a name, version, release, epoch, arch, e.g.::
+        foo-1.0-1.i386.rpm returns foo, 1.0, 1, i386
+        1:bar-9-123a.ia64.rpm returns bar, 9, 123a, 1, ia64
+    """
+
+    if filename[-4:] == '.rpm':
+        filename = filename[:-4]
+
+    archIndex = filename.rfind('.')
+    arch = filename[archIndex+1:]
+
+    relIndex = filename[:archIndex].rfind('-')
+    rel = filename[relIndex+1:archIndex]
+
+    verIndex = filename[:relIndex].rfind('-')
+    ver = filename[verIndex+1:relIndex]
+
+    epochIndex = filename.find(':')
+    if epochIndex == -1:
+        epoch = ''
+    else:
+        epoch = filename[:epochIndex]
+
+    name = filename[epochIndex + 1:verIndex]
+    return name, ver, rel, epoch, arch
 
 
 def parse_package_name(pkg):
