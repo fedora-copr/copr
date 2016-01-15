f58b0770815a633aacef40b5f866d08b325d367c
Adam Samalik <asamalik@redhat.com>
2016-01-15 14:13:03 +0100

    [frontend] recent pages

--- frontend/coprs_frontend/coprs/views/recent_ns/recent_general.py
+++ frontend/coprs_frontend/coprs/views/recent_ns/recent_general.py
@@ -2,6 +2,7 @@ import flask
 
 from . import recent_ns
 from coprs.logic import builds_logic
+from coprs.views.misc import login_required
 
 import logging
 
@@ -9,7 +10,8 @@ log = logging.getLogger(__name__)
 
 
 @recent_ns.route("/")
-def recent():
+@recent_ns.route("/all/")
+def all():
     # tasks = bilds_logic.BuildsLogic.get_build_tasks(
     builds = builds_logic.BuildsLogic.get_recent_tasks(limit=20)
     # if flask.g:
@@ -19,7 +21,24 @@ def recent():
     else:
         user_builds = []
 
-    return flask.render_template("recent.html",
+    return flask.render_template("recent/all.html",
+                                 number=len(list(builds)),
+                                 builds=builds,
+                                 user_builds=user_builds)
+
+@recent_ns.route("/my/")
+@login_required
+def my():
+    # tasks = bilds_logic.BuildsLogic.get_build_tasks(
+    builds = builds_logic.BuildsLogic.get_recent_tasks(limit=20)
+    # if flask.g:
+    #    log.info("flask.g")\
+    if flask.g.user is not None:
+        user_builds = builds_logic.BuildsLogic.get_recent_tasks(user=flask.g.user, limit=20)
+    else:
+        user_builds = []
+
+    return flask.render_template("recent/my.html",
                                  number=len(list(builds)),
                                  builds=builds,
                                  user_builds=user_builds)
