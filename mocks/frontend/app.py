e7c3e9dc1114a01d69e22f38e556701601238ee6
clime <clime@redhat.com>
2016-03-30 17:46:48 +0200

    [copr-mocks-frontend] fixes & debug infos & better error/input/output handling

--- mocks/frontend/app.py
+++ mocks/frontend/app.py
@@ -5,6 +5,8 @@ import json
 import sys
 import os
 
+datadir = './data'
+
 app = flask.Flask(__name__)
 
 import_task_dict = {}
@@ -15,7 +17,8 @@ backend_failed_posts = []
 backend_succeeded_posts = []
 backend_starting_posts = []
 backend_running_posts = []
-backend_other_posts = [] # should be empty
+backend_other_posts = [] # should stay empty
+
 
 # from in backend/constants.py
 class BuildStatus(object):
@@ -34,14 +37,41 @@ def shutdown_server():
     func()
 
 
+def debug_output(data, label='RECEIVED:', delim=True):
+    if delim:
+        print('---------------')
+    print(label)
+    print(data)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    app.logger.error('Server Error: %s', str(error))
+    return 'NOT OK', 500
+
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    app.logger.error('Unhandled Exception: %s', str(e))
+    return 'NOT OK', 500
+
+
 @app.route('/tmp/<path:path>')
 def serve_uploaded_file(path):
     return app.send_static_file(path)
 
 
+@app.route('/api/coprs/<path:path>/detail/')
+def backend_auto_createrepo_status(path):
+    return flask.jsonify({
+        'detail': { 'auto_createrepo': True }
+    })
+
+
 @app.route('/backend/importing/')
 def dist_git_importing_queue():
     response = {'builds': list(import_task_dict.values())}
+    debug_output(response, 'SENDING:')
     return flask.jsonify(response)
 
 
@@ -56,74 +86,76 @@ def dist_git_upload_completed():
 @app.route('/backend/waiting/', methods=['GET'])
 def backend_waiting_queue():
     response = {'actions': [], 'builds': list(waiting_task_dict.values())}
+    debug_output(response, 'SENDING:')
     return flask.jsonify(response)
 
 
-@app.route("/backend/starting_build/", methods=["POST", "PUT"])
+@app.route('/backend/starting_build/', methods=['POST', 'PUT'])
 def starting_build():
-    print(flask.request.json)
-    response = flask.request.json
-    task_id = '{0}-{1}'.format(response['build_id'], response['chroot'])
+    debug_output(flask.request.json, 'RECEIVED:')
+    update = flask.request.json
+    task_id = '{0}-{1}'.format(update['build_id'], update['chroot'])
 
-    backend_starting_posts.append(response)
+    backend_starting_posts.append(update)
     waiting_task_dict.pop(task_id)
 
-    return flask.jsonify({ "can_start": True })
+    response = {'can_start': True}
+    debug_output(response, 'SENDING BACK:', delim=False)
+    return flask.jsonify(response)
 
 
-@app.route("/backend/update/", methods=["POST", "PUT"])
-def update():
-    print(flask.request.json)
-    response = flask.request.json
-    build = response['builds'][0]
+@app.route('/backend/update/', methods=['POST', 'PUT'])
+def backend_update():
+    debug_output(flask.request.json, 'RECEIVED:')
+    update = flask.request.json
+    build = update['builds'][0]
 
     if build['status'] == BuildStatus.RUNNING:
-        backend_running_posts.append(build)
-    elif build['status'] == BuildStatus.SUCEEDED:
-        backend_succeeded_posts.append(build)
+        backend_running_posts.append(update)
+    elif build['status'] == BuildStatus.SUCCEEDED:
+        backend_succeeded_posts.append(update)
         test_for_server_end()
-    elif build['status'] == BuildStatus.FAILED:
-        backend_failed_posts.append(build)
+    elif build['status'] == BuildStatus.FAILURE:
+        backend_failed_posts.append(update)
         test_for_server_end()
     else:
-        backend_other_posts.append(build) # should stay empty
+        backend_other_posts.append(update) # should stay empty
 
-    return flask.jsonify({})
-
-
-@app.route("/backend/reschedule_all_running/", methods=["POST"])
-def reschedule_all_running():
-    return "OK", 200
+    response = {}
+    debug_output(response, 'SENDING BACK:', delim=False)
+    return flask.jsonify(response)
 
 
-@app.route("/backend/reschedule_build_chroot/", methods=["POST", "PUT"])
-def reschedule_build_chroot():
+@app.route('/backend/reschedule_all_running/', methods=['POST'])
+def backend_reschedule_all_running():
+    return 'OK', 200
+
+
+@app.route('/backend/reschedule_build_chroot/', methods=['POST', 'PUT'])
+def backend_reschedule_build_chroot():
     return flask.jsonify({})
 
 
 def load_import_tasks():
-    with open('data/import-tasks.json', 'r') as f:
+    inputfile = '{0}/in/import-tasks.json'.format(datadir)
+    with open(inputfile, 'r') as f:
         task_queue = json.loads(f.read())
         for task in task_queue:
             import_task_dict[task['task_id']] = task
-    print(import_task_dict)
-    print('--------------')
+    debug_output(import_task_dict, 'LOADED IMPORT TASKS:', delim=False)
 
 
 def load_waiting_tasks():
-    with open('data/waiting-tasks.json', 'r') as f:
+    inputfile = '{0}/in/waiting-tasks.json'.format(datadir)
+    with open(inputfile, 'r') as f:
         task_queue = json.loads(f.read())
         for task in task_queue:
             waiting_task_dict[task['task_id']] = task
-    print(waiting_task_dict)
-    print('---------------')
+    debug_output(waiting_task_dict, 'LOADED WAITING TASKS:', delim=False)
 
 
 def dump_responses():
-    if len(sys.argv) > 1:
-        ouput_dir = sys.argv[1]
-    else:
-        output_dir = 'data'
+    outputdir = '{0}/out'.format(datadir)
 
     output = {
         'distgit_responses': distgit_responses,
@@ -146,6 +178,8 @@ def test_for_server_end():
 
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        datadir = sys.argv[1]
     load_import_tasks()
     load_waiting_tasks()
     app.run()
