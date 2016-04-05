46dfcf06deba9e4dd7a0d477c9aeb955b039e5d7
clime <clime@redhat.com>
2016-04-05 15:32:11 +0200

    [copr-mocks-frontend] only finished (failed/succeeded) builds are dumped in the end
    We won't test the "build progress" but only the final build results (i.e. existing repository with the built package).

--- mocks/frontend/app.py
+++ mocks/frontend/app.py
@@ -5,6 +5,8 @@ import json
 import sys
 import os
 
+########################### constants & vars ###########################
+
 datadir = './data'
 
 app = flask.Flask(__name__)
@@ -13,12 +15,7 @@ import_task_dict = {}
 waiting_task_dict = {}
 
 distgit_responses = []
-backend_failed_posts = []
-backend_succeeded_posts = []
-backend_starting_posts = []
-backend_running_posts = []
-backend_other_posts = [] # should stay empty
-
+backend_builds = []
 
 # from in backend/constants.py
 class BuildStatus(object):
@@ -28,59 +25,48 @@ class BuildStatus(object):
     PENDING = 4
     SKIPPED = 5
 
-
-# http://flask.pocoo.org/snippets/67/
-def shutdown_server():
-    func = flask.request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-
-
-def debug_output(data, label='RECEIVED:', delim=True):
-    if delim:
-        print('---------------')
-    print(label)
-    print(data)
-
+########################### error handling ###########################
 
 @app.errorhandler(500)
 def internal_server_error(error):
-    print('Server Error: %s', str(error))
+    print('Server Error: %s' % str(error))
     return 'NOT OK', 500
 
 
 @app.errorhandler(Exception)
 def unhandled_exception(e):
-    print('Unhandled Exception: %s', str(e))
+    print('Unhandled Exception: %s' % str(e))
     return 'NOT OK', 500
 
+########################### file server interface ###########################
 
 @app.route('/tmp/<path:path>')
 def serve_uploaded_file(path):
     return app.send_static_file(path)
 
-
-@app.route('/api/coprs/<path:path>/detail/')
-def backend_auto_createrepo_status(path):
-    return flask.jsonify({
-        'detail': { 'auto_createrepo': True }
-    })
-
+########################### distgit interface ###########################
 
 @app.route('/backend/importing/')
-def dist_git_importing_queue():
+def distgit_importing_queue():
     response = {'builds': list(import_task_dict.values())}
     debug_output(response, 'SENDING:')
     return flask.jsonify(response)
 
 
 @app.route('/backend/import-completed/', methods=['POST', 'PUT'])
-def dist_git_upload_completed():
+def distgit_upload_completed():
     distgit_responses.append(flask.request.json)
     import_task_dict.pop(flask.request.json['task_id'])
     test_for_server_end()
     return flask.jsonify({'updated': True})
+
+########################### backend interface ###########################
+
+@app.route('/api/coprs/<path:path>/detail/')
+def backend_auto_createrepo_status(path):
+    return flask.jsonify({
+        'detail': { 'auto_createrepo': True }
+    })
 
 
 @app.route('/backend/waiting/', methods=['GET'])
@@ -91,12 +77,11 @@ def backend_waiting_queue():
 
 
 @app.route('/backend/starting_build/', methods=['POST', 'PUT'])
-def starting_build():
+def backend_starting_build():
     debug_output(flask.request.json, 'RECEIVED:')
     update = flask.request.json
     task_id = '{0}-{1}'.format(update['build_id'], update['chroot'])
 
-    backend_starting_posts.append(update)
     waiting_task_dict.pop(task_id)
 
     response = {'can_start': True}
@@ -108,18 +93,11 @@ def starting_build():
 def backend_update():
     debug_output(flask.request.json, 'RECEIVED:')
     update = flask.request.json
-    build = update['builds'][0]
 
-    if build['status'] == BuildStatus.RUNNING:
-        backend_running_posts.append(update)
-    elif build['status'] == BuildStatus.SUCCEEDED:
-        backend_succeeded_posts.append(update)
-        test_for_server_end()
-    elif build['status'] == BuildStatus.FAILURE:
-        backend_failed_posts.append(update)
-        test_for_server_end()
-    else:
-        backend_other_posts.append(update) # should stay empty
+    for build in update['builds']:
+        if build['status'] == BuildStatus.FAILURE or build['status'] == BuildStatus.SUCCEEDED: # if build is finished
+            backend_builds.append(build)
+            test_for_server_end()
 
     response = {}
     debug_output(response, 'SENDING BACK:', delim=False)
@@ -135,6 +113,7 @@ def backend_reschedule_all_running():
 def backend_reschedule_build_chroot():
     return flask.jsonify({})
 
+########################### helpers ###########################
 
 def load_import_tasks():
     inputfile = '{0}/in/import-tasks.json'.format(datadir)
@@ -160,17 +139,12 @@ def dump_responses():
 
     output = {
         'distgit_responses': distgit_responses,
-        'backend_failed_posts': backend_failed_posts,
-        'backend_succeeded_posts': backend_succeeded_posts,
-        'backend_starting_posts': backend_starting_posts,
-        'backend_running_posts': backend_running_posts,
-        'backend_other_posts': backend_other_posts,
+        'backend_builds': backend_builds,
     }
 
     for filename, data in output.items():
         with open(os.path.join(outputdir, filename), 'w') as f:
-            #f.write(json.dumps(data))
-            f.write('1') # just write 1 for each entry for the time being (it is still a simple test of the communication protocol, though the message contents won't be tested)
+            f.write(json.dumps(data))
 
 
 def test_for_server_end():
@@ -178,6 +152,22 @@ def test_for_server_end():
         dump_responses()
         shutdown_server()
 
+
+# http://flask.pocoo.org/snippets/67/
+def shutdown_server():
+    func = flask.request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+def debug_output(data, label='RECEIVED:', delim=True):
+    if delim:
+        print('---------------')
+    print(label)
+    print(data)
+
+########################### main ###########################
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
