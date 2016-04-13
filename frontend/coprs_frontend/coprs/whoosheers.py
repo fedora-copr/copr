7588cac8d98d8aff5fab1a00a76e7a07551c378b
clime <clime@redhat.com>
2016-04-13 12:19:29 +0200

    [frontend] search improvements
    For the improvements to take effect, call ./manage.py update_indexes
    and use flask-whooshee with https://github.com/bkabrda/flask-whooshee/pull/13 merged.

--- frontend/coprs_frontend/coprs/whoosheers.py
+++ frontend/coprs_frontend/coprs/whoosheers.py
@@ -4,19 +4,20 @@ from flask.ext.whooshee import AbstractWhoosheer
 
 from coprs import models
 from coprs import whooshee
-
+from coprs import db
 
 @whooshee.register_whoosheer
 class CoprUserWhoosheer(AbstractWhoosheer):
     schema = whoosh.fields.Schema(
         copr_id=whoosh.fields.NUMERIC(stored=True, unique=True),
         user_id=whoosh.fields.NUMERIC(stored=True),
-        username=whoosh.fields.TEXT(),
+        username=whoosh.fields.TEXT(field_boost=2),
         # treat dash as a normal character - so searching for example
         # "copr-dev" will really search for "copr-dev"
         coprname=whoosh.fields.TEXT(
             analyzer=whoosh.analysis.StandardAnalyzer(
-                expression=r"\w+(-\.?\w+)*")),
+                expression=r"\w+(-\.?\w+)*"), field_boost=2),
+        chroots=whoosh.fields.TEXT(field_boost=2),
         description=whoosh.fields.TEXT(),
         instructions=whoosh.fields.TEXT())
 
@@ -34,6 +35,7 @@ class CoprUserWhoosheer(AbstractWhoosheer):
                                user_id=copr.owner.id,
                                username=copr.owner.name,
                                coprname=copr.name,
+                               chroots=cls.get_chroot_info(copr),
                                description=copr.description,
                                instructions=copr.instructions)
 
@@ -48,9 +50,26 @@ class CoprUserWhoosheer(AbstractWhoosheer):
                             user_id=copr.owner.id,
                             username=copr.owner.name,
                             coprname=copr.name,
+                            chroots=cls.get_chroot_info(copr),
                             description=copr.description,
                             instructions=copr.instructions)
 
     @classmethod
     def delete_copr(cls, writer, copr):
         writer.delete_by_term("copr_id", copr.id)
+
+
+    @classmethod
+    def get_chroot_info(cls, copr):
+        # NOTE: orm db session for Copr model is already commited at the point insert_*/update_* methods are called.
+        # Hence we use db.engine directly (for a new session).
+        result = db.engine.execute(
+            """
+            SELECT os_release, os_version, arch
+            FROM mock_chroot
+            JOIN copr_chroot ON copr_chroot.mock_chroot_id=mock_chroot.id
+            WHERE copr_chroot.copr_id={0}
+            """.format(copr.id)
+        )
+        return ["{}-{}-{}".format(t[0], t[1], t[2]) for t in result.fetchall()]
+
