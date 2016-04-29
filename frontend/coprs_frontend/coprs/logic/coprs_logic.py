fedora-copr/copr

commit 5e102adb07e26b8738166b994a6fc5d74b8ae589

[frontend][python][cli] allow creating group projects via CLI

frontend/coprs_frontend/coprs/views/api_ns/api_general.py
@@ -14,6 +14,7 @@ from coprs.helpers import fix_protocol_for_backend
 from coprs.logic.api_logic import MonitorWrapper
 from coprs.logic.builds_logic import BuildsLogic
 from coprs.logic.complex_logic import ComplexLogic
+from coprs.logic.users_logic import UsersLogic
 
 from coprs.views.misc import login_required, api_login_required
 
@@ -103,6 +104,7 @@ def api_new_copr(username):
 
     elif form.validate_on_submit():
         infos = []
+        group = UsersLogic.get_group_by_alias(username[1:]).first() if username[0] == "@" else None
 
         try:
             copr = CoprsLogic.add(
@@ -112,8 +114,9 @@ def api_new_copr(username):
                 selected_chroots=form.selected_chroots,
                 description=form.description.data,
                 instructions=form.instructions.data,
-                check_for_duplicates=True,
+                check_for_duplicates=False,
                 auto_createrepo=True,
+                group=group,
             )
             infos.append("New project was successfully created.")
 

cli/copr_cli/main.py
@@ -230,8 +230,16 @@ class Commands(object):
 
         """
 
+        copr = args.name
+        m = re.match(r"([^/]+)/(.*)", copr)
+        if m:
+            username = m.group(1)
+            copr = m.group(2)
+        else:
+            username = None
+
         result = self.client.create_project(
-            projectname=args.name, description=args.description,
+            username=username, projectname=copr, description=args.description,
             instructions=args.instructions, chroots=args.chroots,
             repos=args.repos, initial_pkgs=args.initial_pkgs)
         print(result.message)

python/copr/client/client.py
@@ -514,13 +514,14 @@ class CoprClient(UnicodeMixin):
         return response
 
     def create_project(
-            self, projectname, chroots,
+            self, username, projectname, chroots,
             description=None, instructions=None,
             repos=None, initial_pkgs=None
     ):
         """ Creates a new copr project
             Auth required.
 
+            :param projectname: User or group name
             :param projectname: Copr project name
             :param chroots: List of target chroots
             :param description: [optional] Project description
@@ -533,8 +534,11 @@ class CoprClient(UnicodeMixin):
                 - text fields: "message"
         """
 
+        if not username:
+            username = self.username
+
         url = "{0}/coprs/{1}/new/".format(
-            self.api_url, self.username)
+            self.api_url, username)
 
         if not chroots:
             raise Exception("You should provide chroots")

commit 4a0603a882300ce34a5025f02965b5c91775a61c

check for duplicities during creating
without this we do not check for duplicities in group projects when creating over API call.
This regression was introduced in commit 5e102adb07e26b8738166b994a6fc5d74b8ae589.

The strange-looking condition
   models.Copr.id != copr.id
needs to be added there because when we add group to copr model (where group is backref) then
sqlalchemy call imlicit flush() so the new Copr object is sudenly visible.
It is nowhere documented, but I find some other people reporting the same behaviour on internet.

frontend/coprs_frontend/coprs/logic/coprs_logic.py
@@ -202,8 +202,7 @@ class CoprsLogic(object):
             copr.group = group
 
         # form validation checks for duplicates
-        CoprsLogic.new(user, copr,
-                       check_for_duplicates=check_for_duplicates)
+        cls.new(user, copr, check_for_duplicates=check_for_duplicates)
         CoprChrootsLogic.new_from_names(copr, selected_chroots)
 
         db.session.flush()
@@ -213,9 +212,16 @@ class CoprsLogic(object):
 
     @classmethod
     def new(cls, user, copr, check_for_duplicates=True):
-        if check_for_duplicates and cls.exists_for_user(user, copr.name).all():
-            raise exceptions.DuplicateException(
-                "Copr: '{0}' already exists".format(copr.name))
+        if check_for_duplicates:
+            if copr.group is None and cls.exists_for_user(user, copr.name).all():
+                raise exceptions.DuplicateException(
+                    "Copr: '{0}/{1}' already exists".format(user.name, copr.name))
+            elif copr.group:
+                db.session.flush()  # otherwise copr.id is not set from sequence
+                if cls.exists_for_group(copr.group, copr.name).filter(models.Copr.id != copr.id).all():
+                    db.session.rollback()
+                    raise exceptions.DuplicateException(
+                        "Copr: '@{0}/{1}' already exists".format(copr.group.name, copr.name))
         db.session.add(copr)
 
     @classmethod

frontend/coprs_frontend/coprs/views/api_ns/api_general.py
@@ -114,7 +114,7 @@ def api_new_copr(username):
                 selected_chroots=form.selected_chroots,
                 description=form.description.data,
                 instructions=form.instructions.data,
-                check_for_duplicates=False,
+                check_for_duplicates=True,
                 auto_createrepo=True,
                 group=group,
             )