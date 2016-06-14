2e7f9ff0b1462a836bcc5847bc53b3124466f4b3
clime <clime7@gmail.com>
2016-06-14 07:51:02 +0200

    [frontend] only display normal tasks in status/waiting + bg tasks cnt

--- frontend/coprs_frontend/coprs/views/status_ns/status_general.py
+++ frontend/coprs_frontend/coprs/views/status_ns/status_general.py
@@ -8,10 +8,11 @@ from coprs import helpers
 @status_ns.route("/")
 @status_ns.route("/waiting/")
 def waiting():
-    tasks = builds_logic.BuildsLogic.get_build_task_queue().limit(200)
+    tasks = builds_logic.BuildsLogic.get_build_task_queue(is_background=False).limit(200)
+    bg_tasks_cnt = builds_logic.BuildsLogic.get_build_task_queue(is_background=True).count()
     return flask.render_template("status/waiting.html",
                                  number=len(list(tasks)),
-                                 tasks=tasks)
+                                 tasks=tasks, bg_tasks_cnt=bg_tasks_cnt)
 
 
 @status_ns.route("/running/")
