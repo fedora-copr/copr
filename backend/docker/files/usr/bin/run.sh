0cc92cf2e699404d135a5cb29c556a2af02d0f4e
clime <clime@redhat.com>
2016-06-15 08:27:42 +0200

    [beaker-tests] fix 127.0.0.1 <-> keygen association for BE

--- backend/docker/files/usr/bin/run.sh
+++ backend/docker/files/usr/bin/run.sh
@@ -2,6 +2,7 @@
 
 export LANG=en_US.UTF-8
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+echo '127.0.0.1 keygen' > /etc/hosts
 echo 4096 > /proc/sys/net/core/somaxconn
+/usr/bin/supervisord -c /etc/supervisord.conf
 /bin/bash
