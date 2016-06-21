ca8650e9f570aa12c790064ebca79c83c14c9ed5
Miroslav Suchý <msuchy@redhat.com>
2016-06-21 11:11:28 +0200

    continue from here

--- misc/rubygems.py
+++ misc/rubygems.py
@@ -26,7 +26,7 @@ def submit_build(copr, gem):
 
 def main():
     for gem in all_gems():
-        if gem <= "console-glitter":
+        if gem <= "fotki-export":
             continue
         print("Submitting gem {0}".format(gem))
         submit_build("{}/{}".format(USER, COPR), gem)
