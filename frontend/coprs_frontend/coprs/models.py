9e7bab809fca96553be72ce11169d0dc5b99e668
Jakub Kadlčík <jkadlcik@redhat.com>
2016-11-28 16:17:48 +0100

    [frontend] temporarily use F24 until custom chroot works

--- frontend/coprs_frontend/coprs/models.py
+++ frontend/coprs_frontend/coprs/models.py
@@ -1173,7 +1173,8 @@ class Module(db.Model, helpers.Serializer):
         return Action.query.filter(Action.object_type == "module").filter(Action.object_id == self.id).first()
 
     def repo_url(self, arch):
+        # @TODO Use custom chroot instead of fedora-24
         # @TODO Get rid of OS name from module path, see how koji does it
         # https://kojipkgs.stg.fedoraproject.org/repos/module-base-runtime-0.25-9/latest/x86_64/toplink/packages/module-build-macros/0.1/
-        module_dir = "fedora-rawhide-{}+{}-{}-{}".format(arch, self.name, self.stream, self.version)
+        module_dir = "fedora-24-{}+{}-{}-{}".format(arch, self.name, self.stream, self.version)
         return "/".join([self.copr.repo_url, "modules", module_dir, "latest", arch])
