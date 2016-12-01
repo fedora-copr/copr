5cdf0a630ab0d3859ee6cb411705057f72990ea3
clime <clime@redhat.com>
2016-12-01 05:04:09 +0100

    [beaker-tests-backend] make timeout in cancel_build test more benevolent
    Also change id of cancel_build build task not to overlap with other tests

--- beaker-tests/Regression/backend/tests/cancel_build/run.sh
+++ beaker-tests/Regression/backend/tests/cancel_build/run.sh
@@ -21,7 +21,7 @@ rlJournalStart
         done
 
         kill -9 `pgrep -f app.py` # kill app.py so that it does not wait for build end
-        sleep 40 # sleep additional 40 seconds for the build to get actually started
+        sleep 60 # sleep additional 60 seconds for the build to get running
 
         # test that the build is running
         rlRun "docker exec copr-backend copr_get_vm_info.py | grep -E 'task_id: 10-fedora-24-x86_64'"
@@ -30,7 +30,7 @@ rlJournalStart
         rlRun "/usr/share/copr/mocks/frontend/app.py $TESTPATH $TESTPATH/static" 0
 
         # backend will attempt to contact us now before releasing the vm
-        rlRun "timeout 25 $TESTPATH/respond_200_to_backend.py" 124
+        rlRun "timeout 30 $TESTPATH/respond_200_to_backend.py" 124
 
         # basic outcomes test
         rlRun "jq -e -n --argfile a $IN --argfile b $OUT\
