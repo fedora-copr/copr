52feedb5956406f8bb1529b95ef0f3b24130e641
clime <clime@redhat.com>
2017-02-26 21:06:24 +0100

    [beaker-tests-backend] only append to /etc/hosts, do not overwrite
    NOTE: this now causes problems with "localhost" name resolution. Interesting
    that it worked before.

--- backend/docker/files/usr/bin/run.sh
+++ backend/docker/files/usr/bin/run.sh
@@ -2,7 +2,7 @@
 
 export LANG=en_US.UTF-8
 
-echo '127.0.0.1 keygen' > /etc/hosts
+echo '127.0.0.1 keygen' >> /etc/hosts
 echo 4096 > /proc/sys/net/core/somaxconn
 /usr/bin/supervisord -c /etc/supervisord.conf
 rngd -r /dev/urandom # using /dev/urandom is a hack just for devel, /dev/hwrandom or /dev/hwrng should be used in production
