1dc3cf4cd37b48927adb7f3ecde65868111e08cf
clime <clime@redhat.com>
2017-03-28 17:47:26 -0400

    [beaker-test-backend] add test for detached-builds
    Note: the test is very experimental and does not need to pass in release testing

---
+++ beaker-tests/Regression/backend/tests/detached-builds/run.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+. /usr/bin/rhts-environment.sh || exit 1
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+export TESTPATH="$( builtin cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export IN=$TESTPATH/build-tasks.json
+export OUT=$TESTPATH/build-results.out.json
+
+rlJournalStart
+    rlPhaseStartSetup
+        docker exec copr-backend /bin/sh -c 'dnf -y install procps-ng'
+    rlPhaseEnd
+
+    rlPhaseStartTest TestTemplate
+        rlRun "/usr/share/copr/mocks/frontend/app.py $TESTPATH $TESTPATH/static &" 0
+
+        # wait until build has been retrieved by backend
+        while ! curl --silent http://localhost:5000/backend/waiting/ | grep '"build": null' ; do
+            sleep 1
+        done
+
+	echo "sleep 8"
+        sleep 8 # Here backend will attempt to contact us that the job's state changed to 'running'
+        kill -9 `pgrep -f app.py` # kill app.py so that it does not wait for build end
+	echo "sleep 8"
+        sleep 8 # downloading srpm to builder is taking place, mockchain is not running yet
+
+        PID1=`docker exec copr-backend /bin/sh -c 'pgrep mockchain'`
+	echo "pid1:" $PID1
+
+	echo "Restart backend"
+        docker exec copr-backend /bin/sh -c 'supervisorctl restart all'
+
+        rlRun "/usr/share/copr/mocks/frontend/app.py $TESTPATH $TESTPATH/static &" 0
+
+        # wait until build has been retrieved by backend
+        while ! curl --silent http://localhost:5000/backend/waiting/ | grep '"build": null' ; do
+            sleep 1
+        done
+
+        sleep 4 # Here backend will attempt to contact us that the job's state changed to 'running'
+
+        PID2=`docker exec copr-backend /bin/sh -c 'pgrep mockchain'`
+	echo "pid2:"$PID2
+
+        rlAssertEquals "Test that mockchain has been kept running" $PID1 $PID2
+
+        # wait until build has been retrieved by backend
+        while pgrep -f app.py ; do
+            sleep 1
+        done
+
+        # basic outcomes test
+        rlRun "jq -e -n --argfile a $IN --argfile b $OUT\
+            '(\$a | sort_by(.build_id) | map({build_id: .build_id, status: (if (._expected_outcome == \"success\") then 1 else 0 end)})) ==\
+            (\$b | sort_by(.build_id) | map({build_id: .build_id, status: .status}))'" 0 "Compare expected and actual build outcomes (success/fail)."
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+    	docker exec copr-backend /bin/sh -c 'rm -r /var/tmp/mockremote*'
+    rlPhaseEnd
+rlJournalEnd &> /dev/null
