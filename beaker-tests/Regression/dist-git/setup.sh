91b24d67dedbae764c5f6ce5df8ed86d5d770506
clime <clime@redhat.com>
2017-04-06 13:49:30 +0200

    [dist-git] fix tests both unit and beaker

--- beaker-tests/Regression/dist-git/setup.sh
+++ beaker-tests/Regression/dist-git/setup.sh
@@ -16,6 +16,8 @@ if [[ ! $RELEASETEST ]]; then
 	dnf -y copr enable @copr/copr-dev
 fi
 
+dnf -y copr enable clime/dist-git
+
 ./create_loopback_devices_for_docker.sh # hack for running tests inside docker
 
 dnf -y update
@@ -26,115 +28,9 @@ dnf -y install pyp2rpm
 dnf -y install pyrpkg
 dnf -y install jq
 dnf -y install copr-mocks
-
-ssh-keygen -f /root/.ssh/id_rsa -N '' -q < /dev/zero &> /dev/null
-
-if [ ! -e /root/.ssh/config ]; then
-    echo "\
-Host *
-StrictHostKeyChecking no
-UserKnownHostsFile /dev/null
-    " | tee /root/.ssh/config && chmod 600 /root/.ssh/config
-fi
-
-if ! rpm -qa | grep '^dist-git'; then
-    dnf -y install dist-git
-    dnf -y install dist-git-selinux
-    echo "\
-alias /lookaside /var/lib/dist-git/cache/lookaside
-<Directory /var/lib/dist-git/cache/lookaside>
-    Options Indexes FollowSymLinks
-    AllowOverride None
-    Require all granted
-</Directory>
-    " | tee /etc/httpd/conf.d/dist-git/lookaside.conf
-    echo "\
-AliasMatch \"/repo(/.*)/md5(/.*)\" \"/var/lib/dist-git/cache/lookaside\$1\$2\"
-Alias /repo/ /var/lib/dist-git/cache/lookaside/
-    " | tee /etc/httpd/conf.d/dist-git/lookaside-copr.conf
-    echo "\
-[acls]
-user_groups=cvsadmin
-admin_groups=cvsadmin
-active_branches=el5,el6,el7,epel7,f21,f22,f23,master
-reserved_branches=f[0-9][0-9],epel[0-9],epel[0-9][0-9],el[0-9],olpc[0-9]
-pkgdb_acls_url=
-pkgdb_groups_url=
-
-[notifications]
-email_domain=example.com
-pkg_owner_emails=$PACKAGE-owner@example.com,commits@lists.example.com
-
-[git]
-default_branch_author=Copr Dist Git <copr-devel@lists.fedoraproject.org>
-    " | tee /etc/dist-git/dist-git.conf && chmod 644 /etc/dist-git/dist-git.conf
-    chown root:packager /var/lib/dist-git/git && chmod 775 /var/lib/dist-git/git
-    groupadd cvsadmin
-fi
-
-if ! rpm -qa | grep copr-dist-git; then
-    dnf -y install copr-dist-git
-    echo "\
-[dist-git]
-frontend_base_url=http://localhost:5000
-frontend_auth=1234
-    " | tee /etc/copr/copr-dist-git.conf && chmod 644 /etc/copr/copr-dist-git.conf
-fi
-
-if ! rpm -qa | grep cgit; then
-    dnf -y install cgit
-    sed -e s/^cache-size.*// /etc/cgitrc | tee /etc/cgitrc
-    echo 'project-list=/var/lib/copr-dist-git/cgit_pkg_list' | tee -a /etc/cgitrc
-    echo 'scan-path=/var/lib/dist-git/git/rpms' | tee -a /etc/cgitrc
-fi
-
-if ! id -u copr-service &>/dev/null; then
-    useradd copr-service -G packager,apache,mock
-    su - copr-service -c "ssh-keygen -f ~/.ssh/id_rsa -q -N ''"
-    echo "\
-Host *
-StrictHostKeyChecking no
-UserKnownHostsFile /dev/null
-    " | tee /home/copr-service/.ssh/config && chown copr-service:copr-service /home/copr-service/.ssh/config && chmod 600 /home/copr-service/.ssh/config
-    echo "\
-[user]
-email = copr-devel@lists.fedorahosted.org
-name = Copr dist git
-    " | tee /home/copr-service/.gitconfig && chown copr-service:copr-service /home/copr-service/.gitconfig
-    chown copr-service:copr-service /var/log/copr-dist-git
-    chown copr-service:copr-service /var/lib/copr-dist-git
-fi
-
-if ! id -u copr-dist-git &>/dev/null; then
-    useradd copr-dist-git -G cvsadmin,packager
-    su - copr-dist-git -c "mkdir ~/.ssh"
-    echo "command=\"HOME=/var/lib/dist-git/git/ /usr/share/gitolite3/gitolite-shell copr-dist-git\" $(cat /home/copr-service/.ssh/id_rsa.pub)" | su - copr-dist-git -c "tee ~/.ssh/authorized_keys"
-    chmod 700 /home/copr-dist-git/.ssh
-    chmod 600 /home/copr-dist-git/.ssh/authorized_keys
-fi
-
-if ! id -u git &>/dev/null; then
-    useradd git
-    su - git -c "mkdir ~/.ssh; touch ~/.ssh/authorized_keys"
-    chmod 700 /home/git/.ssh
-    chmod 600 /home/git/.ssh/authorized_keys
-    cat /root/.ssh/id_rsa.pub >> /home/git/.ssh/authorized_keys
-    cat /home/copr-service/.ssh/id_rsa.pub >> /home/git/.ssh/authorized_keys
-fi
-
-/usr/share/dist-git/dist_git_sync.sh
-
-cp -rT files/ /
-
-# setting correct permissions
-chown -R git:git /home/git/
-
-# install copr-mocks deps (why not covered by dnf -y builddep?)
-if ! rpm -qa | grep copr-mocks; then
-    dnf -y install python3-flask
-    dnf -y install python3-flask-script
-    dnf -y install python3-devel
-fi
+dnf -y install cgit
+dnf -y install dist-git
+dnf -y install dist-git-selinux
 
 export LANG=en_US.UTF-8
 
@@ -158,7 +54,74 @@ else
 fi
 cd -
 
+# root user settings
+rm -rf /root/.ssh/
+ssh-keygen -f /root/.ssh/id_rsa -N '' -q < /dev/zero &> /dev/null
+
+echo "\
+Host *
+StrictHostKeyChecking no
+UserKnownHostsFile /dev/null
+" | tee /root/.ssh/config && chmod 600 /root/.ssh/config
+
+# dist-git settings (copr-dist-git specific hack to make md5 uris work)
+echo "\
+AliasMatch \"/repo(/.*)/md5(/.*)\" \"/var/lib/dist-git/cache/lookaside\$1\$2\"
+Alias /repo/ /var/lib/dist-git/cache/lookaside/
+" | tee /etc/httpd/conf.d/dist-git/lookaside-copr.conf
+
+# copr-dist-git settings
+echo "\
+[dist-git]
+frontend_base_url=http://localhost:5000
+frontend_auth=1234
+" | tee /etc/copr/copr-dist-git.conf && chmod 644 /etc/copr/copr-dist-git.conf
+
+echo "\
+[user]
+email = copr-devel@lists.fedorahosted.org
+name = Copr dist git
+" | tee /home/copr-dist-git/.gitconfig && chown copr-dist-git:copr-dist-git /home/copr-dist-git/.gitconfig
+
+su - copr-dist-git -c "rm -rf ~/.ssh; ssh-keygen -f ~/.ssh/id_rsa -q -N ''"
+
+echo "\
+Host *
+StrictHostKeyChecking no
+UserKnownHostsFile /dev/null
+" | tee /home/copr-dist-git/.ssh/config && chown copr-dist-git:copr-dist-git /home/copr-dist-git/.ssh/config && chmod 600 /home/copr-dist-git/.ssh/config
+
+# cgit settings
+sed -e s/^cache-size.*// /etc/cgitrc | tee /etc/cgitrc
+echo 'scan-path=/var/lib/dist-git/git/repositories' | tee -a /etc/cgitrc
+
+# git settings
+if ! id -u git &>/dev/null; then
+    useradd git
+fi
+
+su - git -c "rm -rf ~/.ssh; mkdir ~/.ssh; touch ~/.ssh/authorized_keys"
+chmod 700 /home/git/.ssh
+chmod 600 /home/git/.ssh/authorized_keys
+cat /root/.ssh/id_rsa.pub >> /home/git/.ssh/authorized_keys
+cat /home/copr-dist-git/.ssh/id_rsa.pub >> /home/git/.ssh/authorized_keys
+
+# copy test suite file
+cp -rT files/ /
+
+# setting correct permissions
+chown -R git:git /home/git/
+
+# install copr-mocks deps (why not covered by dnf -y builddep?)
+if ! rpm -qa | grep copr-mocks; then
+    dnf -y install python3-flask
+    dnf -y install python3-flask-script
+    dnf -y install python3-devel
+fi
+
 setenforce 0 # for mock-scm in docker, see https://bugzilla.redhat.com/show_bug.cgi?id=1416813
+
+rm -f /run/nologin # fix for docker environment
 
 # enable & start services
 systemctl daemon-reload
