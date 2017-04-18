58b634da31ab98633ba32db01f5261f07d5c8ce3
Jakub Kadlčík <jkadlcik@redhat.com>
2017-04-18 16:13:00 +0200

    [cli] when building module --url or --yaml needs to be selected

--- cli/copr_cli/main.py
+++ cli/copr_cli/main.py
@@ -955,7 +955,7 @@ def setup_parser():
     # module building
     parser_build_module = subparsers.add_parser("build-module", help="Builds a given module in Copr")
     parser_build_module.add_argument("copr", help="The copr repo to list the packages of. Can be just name of project or even in format owner/project.", nargs="?")
-    parser_build_module_mmd_source = parser_build_module.add_mutually_exclusive_group()
+    parser_build_module_mmd_source = parser_build_module.add_mutually_exclusive_group(required=True)
     parser_build_module_mmd_source.add_argument("--url", help="SCM with modulemd file in yaml format")
     parser_build_module_mmd_source.add_argument("--yaml", help="Path to modulemd file in yaml format")
     parser_build_module.set_defaults(func="action_build_module")
