4b8ed0853c63406ce2ca4867e0fa1ab8470d9499
clime <clime@redhat.com>
2017-06-14 04:17:51 +0200

    [beaker-tests-backend] detached build test fixups

--- beaker-tests/Regression/backend/tests/detached-builds/run.sh
+++ beaker-tests/Regression/backend/tests/detached-builds/run.sh
@@ -21,43 +21,39 @@ rlJournalStart
         done
 
 	echo "sleep 8"
-        sleep 8 # Here backend will attempt to contact us that the job's state changed to 'running'
-        kill -9 `pgrep -f app.py` # kill app.py so that it does not wait for build end
+    sleep 8 # Here backend will attempt to contact us that the job's state changed to 'running'
+    kill -9 `pgrep -f app.py` # kill app.py so that it does not wait for build end
 	echo "sleep 8"
-        sleep 8 # downloading srpm to builder is taking place, mockchain is not running yet
+    sleep 8 # downloading srpm to builder is taking place, copr-rpmbuild is not running yet
 
-        PID1=`docker exec copr-backend /bin/sh -c 'pgrep mockchain'`
+    PID1=`docker exec copr-backend /bin/sh -c 'cat /var/lib/copr-rpmbuild/pid'`
 	echo "pid1:" $PID1
 
 	echo "Restart backend"
-        docker exec copr-backend /bin/sh -c 'supervisorctl restart all'
+    docker exec copr-backend /bin/sh -c 'supervisorctl restart all'
 
-        rlRun "/usr/share/copr/mocks/frontend/app.py $TESTPATH $TESTPATH/static &" 0
+    rlRun "/usr/share/copr/mocks/frontend/app.py $TESTPATH $TESTPATH/static &" 0
 
-        # wait until build has been retrieved by backend
-        while ! curl --silent http://localhost:5000/backend/waiting/ | grep '"build": null' ; do
-            sleep 1
-        done
+    # wait until build has been retrieved by backend
+    while ! curl --silent http://localhost:5000/backend/waiting/ | grep '"build": null' ; do
+        sleep 1
+    done
 
-        sleep 4 # Here backend will attempt to contact us that the job's state changed to 'running'
+    sleep 4 # Here backend will attempt to contact us that the job's state changed to 'running'
 
-        PID2=`docker exec copr-backend /bin/sh -c 'pgrep mockchain'`
+    PID2=`docker exec copr-backend /bin/sh -c 'cat /var/lib/copr-rpmbuild/pid'`
 	echo "pid2:"$PID2
 
-        rlAssertEquals "Test that mockchain has been kept running" $PID1 $PID2
+    rlAssertEquals "Test that copr-rpmbuild has been kept running" $PID1 $PID2
 
-        # wait until build has been retrieved by backend
-        while pgrep -f app.py ; do
-            sleep 1
-        done
+    # wait for the build to be (successfully) finished
+    while pgrep -f app.py ; do
+        sleep 1
+    done
 
-        # basic outcomes test
-        rlRun "jq -e -n --argfile a $IN --argfile b $OUT\
-            '(\$a | sort_by(.build_id) | map({build_id: .build_id, status: (if (._expected_outcome == \"success\") then 1 else 0 end)})) ==\
-            (\$b | sort_by(.build_id) | map({build_id: .build_id, status: .status}))'" 0 "Compare expected and actual build outcomes (success/fail)."
-    rlPhaseEnd
-
-    rlPhaseStartCleanup
-    	docker exec copr-backend /bin/sh -c 'rm -r /var/tmp/mockremote*'
+    # basic outcomes test
+    rlRun "jq -e -n --argfile a $IN --argfile b $OUT\
+        '(\$a | sort_by(.build_id) | map({build_id: .build_id, status: (if (._expected_outcome == \"success\") then 1 else 0 end)})) ==\
+        (\$b | sort_by(.build_id) | map({build_id: .build_id, status: .status}))'" 0 "Compare expected and actual build outcomes (success/fail)."
     rlPhaseEnd
 rlJournalEnd &> /dev/null
