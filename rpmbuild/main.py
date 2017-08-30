fc5f07d10dcaadb87ea9e71078ecae9741a2df48
Jakub Kadlcik <frostyx@email.cz>
2017-08-30 20:52:26 +0200

    [rpmbuild] implement the distgit provider

--- rpmbuild/main.py
+++ rpmbuild/main.py
@@ -1,3 +1,51 @@
+import re
+import os
+import subprocess
+import munch
+import logging
+import tempfile
+from jinja2 import Environment, FileSystemLoader
+
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
+
+log = logging.getLogger(__name__)
+
+
+def run_cmd(cmd, cwd='.', raise_on_error=True):
+    """
+    Runs given command in a subprocess.
+
+    :param list(str) cmd: command to be executed and its arguments
+    :param str workdir: In which directory to execute the command
+    :param bool raise_on_error: if PackageImportException should be raised on error
+
+    :raises PackageImportException
+    :returns munch.Munch(cmd, stdout, stderr, returncode)
+    """
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
+    try:
+        (stdout, stderr) = process.communicate()
+    except OSError as e:
+        raise RuntimeError(str(e))
+
+    result = munch.Munch(
+        cmd = cmd,
+        stdout = stdout.strip(),
+        stderr = stderr.strip(),
+        returncode = process.returncode
+    )
+    log.debug(result)
+
+    if result.returncode != 0:
+        raise RuntimeError(result.stderr)
+
+    return result
+
+
 class SourceType:
     LINK = 1
     UPLOAD = 2
@@ -9,17 +57,59 @@ class SourceType:
 
 
 class DistGitProvider(object):
-    def __init__(self, source_json):
+    def __init__(self, source_json, workdir=None):
         self.clone_url = source_json["clone_url"]
         self.branch = source_json["branch"]
+        self.workdir = workdir
 
     def run(self):
-        pass
+        repodir = os.path.join(self.workdir, "repo")
+        result = self.clone(repodir)
+        log.info(result)
+
+        cfg = self.render_rpkg_template()
+        log.info(cfg)
+
+        config_path = os.path.join(self.workdir, 'fedpkg.conf')
+        f = open(config_path, "w+")
+        f.write(cfg)
+        f.close()
+
+        if self.branch:
+            self.checkout(self.branch, repodir)
+
+        module_name = self.module_name(self.clone_url)
+        result = self.produce_srpm(config_path, module_name, repodir)
+        log.info(result)
+
+    def clone(self, repodir):
+        cmd = ['git', 'clone', self.clone_url, repodir]
+        return run_cmd(cmd)
+
+    def checkout(self, branch, repodir):
+        cmd = ['git', 'checkout', branch]
+        return run_cmd(cmd, cwd=repodir)
+
+    def render_rpkg_template(self):
+        jinja_env = Environment(loader=FileSystemLoader("."))
+        template = jinja_env.get_template('fedpkg.conf.j2')
+        parse = urlparse(self.clone_url)
+        distgit_domain = parse.netloc
+        return template.render(distgit_domain=distgit_domain, scheme=parse.scheme)
+
+    def module_name(self, url):
+        parse = urlparse(url)
+        return re.sub(".git$", "", re.sub("^/c?git/", "", parse.path))
+
+    def produce_srpm(self, config, module_name, repodir):
+        cmd = ["fedpkg", "--config", config, "--module-name", module_name, "srpm"]
+        return run_cmd(cmd, cwd=repodir)
 
 
 def main():
     source_json = None
-    provider = DistGitProvider(source_json)
+    workdir = tempfile.mkdtemp()
+    provider = DistGitProvider(source_json, workdir)
 
 
 if __name__ == "__main__":
--- rpmbuild/tests/test_distgit.py
+++ rpmbuild/tests/test_distgit.py
@@ -1,10 +1,60 @@
 import unittest
+import mock
+import ConfigParser
+import StringIO
 from ..main import DistGitProvider
 
 
 class TestDistGitProvider(unittest.TestCase):
+    def setUp(self):
+        self.source_json = {"clone_url": "https://src.fedoraproject.org/git/rpms/389-admin-console.git",
+                            "branch": "f25"}
+        self.empty_source_json = {"clone_url": None, "branch": None}
+
     def test_init(self):
-        source_json = {"clone_url": "https://src.fedoraproject.org/git/rpms/389-admin-console.git", "branch": "f25"}
-        provider = DistGitProvider(source_json)
+        provider = DistGitProvider(self.source_json)
         self.assertEqual(provider.clone_url, "https://src.fedoraproject.org/git/rpms/389-admin-console.git")
         self.assertEqual(provider.branch, "f25")
+
+    @mock.patch("rpmbuild.main.run_cmd")
+    def test_clone(self, run_cmd):
+        provider = DistGitProvider(self.source_json)
+        provider.clone("/some/repo/directory")
+        assert_cmd = ["git", "clone", "https://src.fedoraproject.org/git/rpms/389-admin-console.git",
+                      "/some/repo/directory"]
+        run_cmd.assert_called_with(assert_cmd)
+
+    @mock.patch("rpmbuild.main.run_cmd")
+    def test_checkout(self, run_cmd):
+        provider = DistGitProvider(self.empty_source_json)
+        provider.checkout("f25", "/some/repo/directory")
+        assert_cmd = ["git", "checkout", "f25"]
+        run_cmd.assert_called_with(assert_cmd, cwd="/some/repo/directory")
+
+    def test_render_rpkg_template(self):
+        provider = DistGitProvider(self.source_json)
+        cfg = provider.render_rpkg_template()
+        parser = ConfigParser.RawConfigParser()
+        parser.readfp(StringIO.StringIO(cfg))
+        self.assertEqual(parser.get("fedpkg", "lookaside"), "https://src.fedoraproject.org/repo/pkgs")
+        self.assertEqual(parser.get("fedpkg", "lookaside_cgi"), "https://src.fedoraproject.org/repo/pkgs/upload.cgi")
+        self.assertEqual(parser.get("fedpkg", "anongiturl"), "git://src.fedoraproject.org/%(module)s")
+
+    def test_module_name(self):
+        provider = DistGitProvider(self.empty_source_json)
+        self.assertEqual(provider.module_name("/git/frostyx/hello/hello.git"), "frostyx/hello/hello")
+        self.assertEqual(provider.module_name("/cgit/frostyx/hello/hello.git"), "frostyx/hello/hello")
+        self.assertEqual(provider.module_name("http://copr-dist-git.fedorainfracloud.org/git/frostyx/hello/hello.git"),
+                         "frostyx/hello/hello")
+
+        self.assertEqual(provider.module_name("/git/rpms/hello.git"), "rpms/hello")
+        self.assertEqual(provider.module_name("https://src.fedoraproject.org/git/rpms/hello.git"),
+                         "rpms/hello")
+
+    @mock.patch("rpmbuild.main.run_cmd")
+    def test_produce_srpm(self, run_cmd):
+        provider = DistGitProvider(self.source_json)
+        provider.produce_srpm("/some/path/to/config", "myself/myproject/mypackage", "/some/repo/directory")
+        assert_cmd = ["fedpkg", "--config", "/some/path/to/config",
+                      "--module-name", "myself/myproject/mypackage", "srpm"]
+        run_cmd.assert_called_with(assert_cmd, cwd="/some/repo/directory")
