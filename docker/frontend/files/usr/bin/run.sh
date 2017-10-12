ad11f73b4422bb5c678cdf424512d4ae8164d71a
Jakub Kadlcik <frostyx@email.cz>
2017-10-12 05:56:10 +0200

    [docker] move as many things as possible to dockerfiles

--- docker/frontend/files/usr/bin/run.sh
+++ docker/frontend/files/usr/bin/run.sh
@@ -4,25 +4,9 @@ export LANG=en_US.UTF-8
 
 /usr/bin/supervisord -c /etc/supervisord.conf
 
-
-
 cd /usr/share/copr/coprs_frontend/ && ./manage.py create_db --alembic alembic.ini
 /usr/share/copr/coprs_frontend/manage.py create_chroot fedora-{22,23,rawhide}-{i386,x86_64,ppc64le} epel-{6,7}-x86_64 epel-6-i386
-#RUN echo 'PEERDNS=no' | tee -a /etc/sysconfig/network
-echo 'nameserver 8.8.8.8' | tee -a /etc/resolv.conf
 
-cat <<EOF >> /etc/httpd/conf.d/copr.conf
-<VirtualHost 0.0.0.0>
-    WSGIPassAuthorization On
-    WSGIDaemonProcess 127.0.0.1 user=copr-fe group=copr-fe threads=5
-    WSGIScriptAlias / /usr/share/copr/coprs_frontend/application
-    WSGIProcessGroup 127.0.0.1
-    <Directory /usr/share/copr>
-        WSGIApplicationGroup %{GLOBAL}
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOF
 
 # selinux: make data dir writeable for httpd
 # TODO: probly correct solution is to uncomment first four lines in
