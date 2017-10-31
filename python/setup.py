2a6a2dae2b6022e5534f0ad6e7e3c258e24c461c
Jun Aruga <jaruga@redhat.com>
2017-10-31 15:55:42 +0100

    Add classifiers to support Python3.

--- python/setup.py
+++ python/setup.py
@@ -56,6 +56,8 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         "Topic :: System :: Archiving :: Packaging",
         "Development Status :: 3 - Alpha",
     ],
