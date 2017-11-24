cab693c8b2470eee2f2eca0c6368898b05489f6b
Jakub Kadlcik <frostyx@email.cz>
2017-11-24 02:54:26 +0100

    [frontend] format the published date

--- frontend/coprs_frontend/coprs/static/js/blog.js
+++ frontend/coprs_frontend/coprs/static/js/blog.js
@@ -2,12 +2,25 @@
 
 $(document).ready(function(){
     $.get("https://fedora-copr.github.io/feed.xml", function (data) {
+
+        // https://stackoverflow.com/a/10996297/3285282
+        Date.prototype.getMonthName = function() {
+            var monthNames = [
+                "Jan", "Feb", "Mar", "Apr", "May", "June",
+                "July", "Aug", "Sept", "Oct", "Nov", "Dec"
+            ];
+            return monthNames[this.getMonth()];
+        }
+
         $(data).find("item").each(function () { // or "item" or whatever suits your feed
             var el = $(this);
+            var published = new Date(el.find("pubDate").text());
+            var f_published = published.getDate() + " " + published.getMonthName() + " " + published.getFullYear()
+
             $("#blog-title").text(el.find("title").text())
             $("#blog-author").text(el.find("author").text())
             $("#blog-link").attr("href", el.find("link").text())
-            $("#blog-date").text(el.find("pubDate").text().split(" ").slice(0, 4).join(" "))
+            $("#blog-date").text(f_published)
             return false
         });
     });
