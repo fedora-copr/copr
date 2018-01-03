c2ca82644c9031a986295be0a6395117f7f559a1
clime <clime@redhat.com>
2018-01-03 22:12:03 +0100

    [frontend] pg#188 COPR webhook doesn't work with branches

--- frontend/coprs_frontend/coprs/views/webhooks_ns/webhooks_general.py
+++ frontend/coprs_frontend/coprs/views/webhooks_ns/webhooks_general.py
@@ -53,7 +53,7 @@ def webhooks_git_push(copr_id, uuid):
     packages = PackagesLogic.get_for_webhook_rebuild(copr_id, uuid, clone_url, commits, ref_type, ref)
 
     for package in packages:
-        BuildsLogic.rebuild_package(package, {'committish': ref})
+        BuildsLogic.rebuild_package(package, {'committish': os.path.basename(ref)})
 
     db.session.commit()
 
@@ -94,7 +94,7 @@ def webhooks_gitlab_push(copr_id, uuid):
     packages = PackagesLogic.get_for_webhook_rebuild(copr_id, uuid, clone_url, commits, ref_type, ref)
 
     for package in packages:
-        BuildsLogic.rebuild_package(package, {'committish': ref})
+        BuildsLogic.rebuild_package(package, {'committish': os.path.basename(ref)})
 
     db.session.commit()
 
