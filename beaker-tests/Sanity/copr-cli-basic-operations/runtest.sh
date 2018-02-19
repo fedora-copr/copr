15039a7d3d250b75fff0010fd3b1c24402eac923
clime <clime@redhat.com>
2018-02-19 20:39:52 +0100

    [sanity-tests] /backend/waiting -> /backend/pending-jobs

--- beaker-tests/Sanity/copr-cli-basic-operations/runtest.sh
+++ beaker-tests/Sanity/copr-cli-basic-operations/runtest.sh
@@ -519,9 +519,9 @@ rlJournalStart
         # Bug 1365882 - on create group copr, gpg key is generated for user and not for group
         WAITING=`mktemp`
         rlRun "copr-cli create ${NAME_PREFIX}Project12 --chroot $CHROOT" 0
-        while :; do curl --silent $FRONTEND_URL/backend/waiting/ > $WAITING; if [ `cat $WAITING |wc -l` -gt 4 ]; then break; fi; done
+        while :; do curl --silent $FRONTEND_URL/backend/pending-jobs > $WAITING; if [ `cat $WAITING |wc -l` -gt 1 ]; then break; fi; done
         cat $WAITING # debug
-        rlRun "cat $WAITING | grep -E '.*data.*username.*' | grep $OWNER" 0
+        rlRun "cat $WAITING | grep -E '.[0].*data.*username.*' | grep $OWNER" 0
 
         # Bug 1368181 - delete-project action run just after delete-build action will bring action_dispatcher down
         # FIXME: this test is not a reliable reproducer. Depends on timing as few others.
