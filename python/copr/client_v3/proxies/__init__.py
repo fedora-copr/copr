24c92d5cdc3bed4bbc8f97f196cdbe831bbd5b4e
Jakub Kadlčík <frostyx@email.cz>
2018-04-05 12:33:19 +0200

    [python] allow to send auth information

--- python/copr/client_v3/proxies/__init__.py
+++ python/copr/client_v3/proxies/__init__.py
@@ -8,3 +8,7 @@ class BaseProxy(object):
     @property
     def api_base_url(self):
         return os.path.join(self.config["copr_url"], "api_3", "")
+
+    @property
+    def auth(self):
+        return self.config["login"], self.config["token"]
--- python/copr/client_v3/requests.py
+++ python/copr/client_v3/requests.py
@@ -15,11 +15,19 @@ class Request(object):
     # This should be a replacement of the _fetch method from APIv1
     # We can have Request, FileRequest, AuthRequest/UnAuthRequest, ...
 
-    def __init__(self, endpoint, api_base_url=None, method=None, data=None):
+    def __init__(self, endpoint, api_base_url=None, method=None, data=None, auth=None):
+        """
+        :param endpoint:
+        :param api_base_url:
+        :param method:
+        :param data:
+        :param auth: tuple (login, token)
+        """
         self.endpoint = endpoint
         self.api_base_url = api_base_url
         self._method = method or GET
         self.data = data
+        self.auth = auth
 
     @property
     def endpoint_url(self):
@@ -30,7 +38,7 @@ class Request(object):
         return self._method.upper()
 
     def send(self):
-        response = requests.request(method=self.method, url=self.endpoint_url, params=self.data)
+        response = requests.request(method=self.method, url=self.endpoint_url, params=self.data, auth=self.auth)
         handle_errors(response.json())
         return Response(headers=response.headers, data=response.json(), request=self)
 
