987b9219b2782c31be751b41829314c24d08117d
Jakub Kadlčík <frostyx@email.cz>
2018-04-09 18:55:29 +0200

    [python] add generator for iterating over all pages

--- python/copr/client_v3/pagination.py
+++ python/copr/client_v3/pagination.py
@@ -15,9 +15,22 @@ def next_page(objects):
     return response.munchify()
 
 
+# @TODO remove all_pages function if unlimited generator is preferred over it
 def all_pages(objects):
     all_objects = []
     while objects:
         all_objects.extend(objects)
         objects = next_page(objects)
     return all_objects
+
+
+def unlimited(objects):
+    if not objects:
+        objects = next_page(objects)
+
+    if not objects:
+        return
+
+    yield objects.pop()
+    for item in unlimited(objects):
+        yield item
