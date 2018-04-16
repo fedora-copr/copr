0fa57fffb1c98e5c6cafd254b02bdbe3721318b3
Jakub Kadlčík <frostyx@email.cz>
2018-04-16 22:45:57 +0200

    [frontend] add method for getting a build config

--- frontend/coprs_frontend/coprs/views/apiv3_ns/apiv3_project_chroots.py
+++ frontend/coprs_frontend/coprs/views/apiv3_ns/apiv3_project_chroots.py
@@ -2,6 +2,8 @@ import flask
 from . import query_params, get_copr
 from coprs.views.apiv3_ns import apiv3_ns
 from coprs.logic.complex_logic import ComplexLogic
+from coprs.helpers import generate_build_config
+from coprs.exceptions import ApiError
 
 
 def to_dict(copr_chroot):
@@ -14,3 +16,13 @@ def get_project_chroot(ownername, projectname, chrootname):
     copr = get_copr()
     chroot = ComplexLogic.get_copr_chroot_safe(copr, chrootname)
     return flask.jsonify(to_dict(chroot))
+
+
+@apiv3_ns.route("/project-chroot/build-config", methods=["GET"])
+@query_params()
+def get_build_config(ownername, projectname, chrootname):
+    copr = get_copr()
+    config = generate_build_config(copr, chrootname)
+    if not config:
+        raise ApiError('Chroot not found.')
+    return flask.jsonify(config)
--- python/copr/client_v3/proxies/project_chroot.py
+++ python/copr/client_v3/proxies/project_chroot.py
@@ -16,3 +16,14 @@ class ProjectChrootProxy(BaseProxy):
         request = Request(endpoint, api_base_url=self.api_base_url, params=params)
         response = request.send()
         return response.munchify()
+
+    def get_build_config(self, ownername, projectname, chrootname):
+        endpoint = "/project-chroot/build-config"
+        params = {
+            "ownername": ownername,
+            "projectname": projectname,
+            "chrootname": chrootname,
+        }
+        request = Request(endpoint, api_base_url=self.api_base_url, params=params)
+        response = request.send()
+        return response.munchify()
