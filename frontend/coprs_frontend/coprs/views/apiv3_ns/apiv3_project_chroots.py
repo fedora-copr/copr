6274c1a1e56e2b0fdc348618d45494eb2dc87f2a
Jakub Kadlčík <frostyx@email.cz>
2018-04-19 15:49:23 +0200

    [frontend][python] add method for editing a chroot

--- frontend/coprs_frontend/coprs/views/apiv3_ns/apiv3_project_chroots.py
+++ frontend/coprs_frontend/coprs/views/apiv3_ns/apiv3_project_chroots.py
@@ -1,9 +1,12 @@
 import flask
-from . import query_params, get_copr
+from . import query_params, get_copr, file_upload
+from coprs.views.misc import api_login_required
 from coprs.views.apiv3_ns import apiv3_ns
 from coprs.logic.complex_logic import ComplexLogic
 from coprs.helpers import generate_build_config
 from coprs.exceptions import ApiError
+from coprs import db, models, forms
+from coprs.logic.coprs_logic import CoprChrootsLogic
 
 
 def to_dict(copr_chroot):
@@ -26,3 +29,31 @@ def get_build_config(ownername, projectname, chrootname):
     if not config:
         raise ApiError('Chroot not found.')
     return flask.jsonify(config)
+
+
+@apiv3_ns.route("/project-chroot/edit", methods=["POST"])
+@file_upload()
+@query_params()
+@api_login_required
+def edit_project_chroot(ownername, projectname, chrootname):
+    copr = get_copr()
+    form = forms.ModifyChrootForm(csrf_enabled=False)
+    chroot = ComplexLogic.get_copr_chroot_safe(copr, chrootname)
+
+    if not form.validate_on_submit():
+        raise ApiError("Invalid request: {0}".format(form.errors))
+
+    buildroot_pkgs = repos = comps_xml = comps_name = None
+    if "buildroot_pkgs" in flask.request.form:
+        buildroot_pkgs = form.buildroot_pkgs.data
+    if "repos" in flask.request.form:
+        repos = form.repos.data
+    if form.upload_comps.has_file():
+        comps_xml = form.upload_comps.data.stream.read()
+        comps_name = form.upload_comps.data.filename
+    if form.delete_comps.data:
+        CoprChrootsLogic.remove_comps(flask.g.user, chroot)
+    CoprChrootsLogic.update_chroot(
+        flask.g.user, chroot, buildroot_pkgs, repos, comps=comps_xml, comps_name=comps_name)
+    db.session.commit()
+    return flask.jsonify(to_dict(chroot))
--- python/copr/client_v3/proxies/project_chroot.py
+++ python/copr/client_v3/proxies/project_chroot.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import os
 from . import BaseProxy
 from ..requests import Request, FileRequest, POST
 
@@ -25,5 +26,27 @@ class ProjectChrootProxy(BaseProxy):
             "chrootname": chrootname,
         }
         request = Request(endpoint, api_base_url=self.api_base_url, params=params)
+        response = request.send()
+        return response.munchify()
+
+    def edit(self, ownername, projectname, chrootname, packages=None, repos=None, comps=None, delete_comps=False):
+        endpoint = "/project-chroot/edit"
+        params = {
+            "ownername": ownername,
+            "projectname": projectname,
+            "chrootname": chrootname,
+        }
+        data = {
+            "repos": " ".join(repos or []),
+            "buildroot_pkgs": " ".join(packages or []),
+            "delete_comps": delete_comps,
+        }
+        files = {}
+        if comps:
+            comps_f = open(comps, "rb")
+            files["upload_comps"] = (os.path.basename(comps_f.name), comps_f, "application/text")
+
+        request = FileRequest(endpoint, api_base_url=self.api_base_url, method=POST,
+                              params=params, data=data, files=files, auth=self.auth)
         response = request.send()
         return response.munchify()
