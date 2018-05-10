f647722d8a7cbe423c2c0af556d8006d6e1aee34
Jakub Kadlčík <frostyx@email.cz>
2018-05-10 15:31:29 +0200

    [python] add docstrings for exceptions

--- python/copr/client_v3/exceptions.py
+++ python/copr/client_v3/exceptions.py
@@ -1,10 +1,19 @@
 class CoprException(Exception):
+    """
+    Base Copr exception
+    """
     pass
 
 
 class CoprRequestException(CoprException):
+    """
+    Raised when the API request doesn't proceed successfully
+    """
     pass
 
 
 class CoprValidationException(CoprException):
+    """
+    Raised when the data sent from client to API are not valid
+    """
     pass
