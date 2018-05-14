bde1e833522b941c310ea5938346dc9b33dbaa32
Jakub Kadlčík <frostyx@email.cz>
2018-05-14 22:14:54 +0200

    [python] fix failing test

--- python/copr/test/client_v3/test_requests.py
+++ python/copr/test/client_v3/test_requests.py
@@ -50,5 +50,5 @@ def test_handle_errors():
     assert handle_errors({"id": 1, "foo": "bar"}) is None
 
     with pytest.raises(CoprRequestException) as ex:
-        handle_errors({"error": 404, "message": "Something wasn't found"})
+        handle_errors({"error": "Something wasn't found"})
     assert ex.value.message == "Something wasn't found"
