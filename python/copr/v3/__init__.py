b29ce135c0adaeab57ea4a042dad9cda0a83bcea
Jakub Kadlčík <frostyx@email.cz>
2018-05-21 20:14:39 +0200

    [python] provide all exceptions

--- python/copr/v3/__init__.py
+++ python/copr/v3/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 from .requests import Request, Response, GET, POST
-from .exceptions import CoprRequestException, CoprValidationException
 from .helpers import config_from_file
 from .client import Client
 from .proxies.general import GeneralProxy
@@ -10,16 +9,26 @@ from .proxies.build import BuildProxy
 from .proxies.package import PackageProxy
 from .proxies.project_chroot import ProjectChrootProxy
 from .proxies.module import ModuleProxy
+from .exceptions import (CoprException,
+                         CoprRequestException,
+                         CoprValidationException,
+                         CoprNoConfException,
+                         CoprConfigException)
 
 
 __all__ = [
     config_from_file,
     Client,
+
     GeneralProxy,
     BuildProxy,
     PackageProxy,
     ProjectChrootProxy,
     ModuleProxy,
+
+    CoprException,
     CoprRequestException,
     CoprValidationException,
+    CoprNoConfException,
+    CoprConfigException,
 ]
