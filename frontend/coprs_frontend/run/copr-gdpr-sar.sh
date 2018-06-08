e556cfb6533db441e92e446cb5c2cfa4facd376f
Jakub Kadlčík <frostyx@email.cz>
2018-06-08 16:37:20 +0200

    [frontend] add SAR retrival script
    See http://fedora-infra-docs.readthedocs.io/en/latest/sysadmin-guide/sops/gdpr_sar.html

---
+++ frontend/coprs_frontend/run/copr-gdpr-sar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Purpose of this script is to be remotely executed from batcave01
+# to collect user SAR data
+# Read more: http://fedora-infra-docs.readthedocs.io/en/latest/sysadmin-guide/sops/gdpr_sar.html
+# Playbook: https://infrastructure.fedoraproject.org/cgit/ansible.git/tree/playbooks/manual/gdpr/sar.yml
+# Usage: SAR_USERNAME=someusername copr-gdpr-sar.sh
+copr-frontend dump_user $SAR_USERNAME
