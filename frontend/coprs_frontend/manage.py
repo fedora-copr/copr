146761450cf9bf0b1ae95793ebca36812241f63b
Pavel Raiskup <praiskup@redhat.com>
2018-09-24 13:37:14 +0200

    [frontend] more robust ./run_tests.sh
    - reuse it in copr-frontend.spec
    - reuse './manage.py tests'
    - add --coverage option into manage.py
    - wait till redis shuts down
    - do 'set -e' to fail asap, and 'set -x' to see logs
    - install 'cleanup' trap to always kill redis
    - put copr_common on PYTHONPATH
    
    Fixes: #415

--- frontend/coprs_frontend/manage.py
+++ frontend/coprs_frontend/manage.py
@@ -25,17 +25,34 @@ from coprs.helpers import chroot_to_branch
 
 class TestCommand(Command):
 
-    def run(self, test_args):
+    def run(self, coverage, test_args):
         os.environ["COPRS_ENVIRON_UNITTEST"] = "1"
         if not (("COPR_CONFIG" in os.environ) and os.environ["COPR_CONFIG"]):
             os.environ["COPR_CONFIG"] = "/etc/copr/copr_unit_test.conf"
-        os.environ["PYTHONPATH"] = "."
-        return subprocess.call(["/usr/bin/python3", "-m", "pytest"] + (test_args or []))
+
+        if 'PYTHONPATH' in os.environ:
+            os.environ['PYTHONPATH'] = os.environ['PYTHONPATH'] + ':.'
+        else:
+            os.environ['PYTHONPATH'] = '.'
+
+        additional_args = []
+
+        if coverage:
+            additional_args.extend([
+                '--cov-report', 'term-missing', '--cov', 'coprs'
+            ])
+
+        return subprocess.call(["/usr/bin/python3", "-m", "pytest"] + additional_args)
 
     option_list = (
         Option("-a",
                dest="test_args",
                nargs=argparse.REMAINDER),
+        Option("--coverage",
+               dest="coverage",
+               required=False,
+               action='store_true',
+               default=False),
     )
 
 
