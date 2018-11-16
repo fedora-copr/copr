e08d24fca5e836abc72c8fe87a60d5778cd68a9d
clime <clime@redhat.com>
2018-11-16 10:59:08 +0100

    [frontend] pg#383 add data migration to remove build.results column

---
+++ frontend/coprs_frontend/alembic/schema/versions/6fed8655d074_remove_build_results_column.py
@@ -0,0 +1,22 @@
+"""remove_build_results_column
+
+Revision ID: 6fed8655d074
+Revises: 51716ab39d37
+Create Date: 2018-11-16 10:40:23.891425
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '6fed8655d074'
+down_revision = '51716ab39d37'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    session = sa.orm.sessionmaker(bind=op.get_bind())()
+    session.execute("ALTER TABLE build DROP COLUMN IF EXISTS results")
+
+def downgrade():
+    pass
