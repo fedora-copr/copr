b00197bf16552a3fde5db7a4e74b808d2b2c6bf5
clime <clime@redhat.com>
2019-01-09 13:09:35 +0100

    [frontend] remove private columns from user and copr

---
+++ frontend/coprs_frontend/alembic/schema/versions/b8a8a1345ed9_remove_now_private_columns_from_user_and_copr.py
@@ -0,0 +1,34 @@
+"""Remove now private columns from user and copr
+
+Revision ID: b8a8a1345ed9
+Revises: 7723d580c625
+Create Date: 2019-01-09 13:03:12.138976
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b8a8a1345ed9'
+down_revision = '7723d580c625'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.drop_column('copr', 'webhook_secret')
+    op.drop_column('copr', 'scm_api_auth_json')
+    op.drop_column('user', 'api_login')
+    op.drop_column('user', 'timezone')
+    op.drop_column('user', 'mail')
+    op.drop_column('user', 'api_token_expiration')
+    op.drop_column('user', 'api_token')
+
+
+def downgrade():
+    op.add_column('user', sa.Column('api_token', sa.VARCHAR(length=40), autoincrement=False, nullable=False))
+    op.add_column('user', sa.Column('api_token_expiration', sa.DATE(), autoincrement=False, nullable=False))
+    op.add_column('user', sa.Column('mail', sa.VARCHAR(length=150), autoincrement=False, nullable=False))
+    op.add_column('user', sa.Column('timezone', sa.VARCHAR(length=50), autoincrement=False, nullable=True))
+    op.add_column('user', sa.Column('api_login', sa.VARCHAR(length=40), autoincrement=False, nullable=False))
+    op.add_column('copr', sa.Column('scm_api_auth_json', sa.TEXT(), autoincrement=False, nullable=True))
+    op.add_column('copr', sa.Column('webhook_secret', sa.VARCHAR(length=100), autoincrement=False, nullable=True))
