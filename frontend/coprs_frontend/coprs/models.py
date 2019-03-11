6a74fff2a22b916e1c325d2e5eb22e7acae1ece5
Jakub Kadlčík <frostyx@email.cz>
2019-03-11 09:38:30 +0100

    [frontend] remove leftover old_status after PR#562
    The PR#562 removed every occurrence of `old_status` from the code
    and removed the DB column, but left the model property, which is
    now causing a SQL error every time when selecting packages.

--- frontend/coprs_frontend/coprs/models.py
+++ frontend/coprs_frontend/coprs/models.py
@@ -508,14 +508,6 @@ class Package(db.Model, helpers.Serializer, CoprSearchRelatedData):
     # enable networking during a build process
     enable_net = db.Column(db.Boolean, default=False, server_default="0", nullable=False)
 
-    # @TODO Remove me few weeks after Copr migration
-    # Contain status of the Package before migration
-    # Normally the `status` is not stored in `Package`. It is computed from `status` variable of `BuildChroot`,
-    # but `old_status` has to be stored here, because we migrate whole `package` table, but only succeeded builds.
-    # Therefore if `old_status` was in `BuildChroot` we wouldn't be able to know old state of non-succeeded packages
-    # even though it would be known before migration.
-    old_status = db.Column(db.Integer)
-
     builds = db.relationship("Build", order_by="Build.id")
 
     # relations
