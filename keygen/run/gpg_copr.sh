b08e0a4cbd29612bdb724de3b3565dc6a2566c26
Pavel Raiskup <praiskup@redhat.com>
2019-05-03 09:59:04 +0200

    [keygen] shell fix s/$@/"$@"/

--- keygen/run/gpg_copr.sh
+++ keygen/run/gpg_copr.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-runuser -u copr-signer  /usr/bin/gpg2 -- --homedir /var/lib/copr-keygen/gnupg --no-auto-check-trustdb  $@
+# TODO: implement this in python, and use app.config for --homedir, etc.
+runuser -u copr-signer \
+    /usr/bin/gpg2 -- --homedir /var/lib/copr-keygen/gnupg --no-auto-check-trustdb "$@"
