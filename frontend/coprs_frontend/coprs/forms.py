fedora-copr/copr

commit 3e7c629460c86dd6ba0603b5f96c43111fe0ef33

[frontend] move "Other options" to separate panel

frontend/coprs_frontend/coprs/forms.py
@@ -230,7 +230,7 @@ class ValueToPermissionNumberFilter(object):
 class CoprFormFactory(object):
 
     @staticmethod
-    def create_form_cls(mock_chroots=None, user=None, group=None):
+    def create_form_cls(mock_chroots=None, user=None, group=None, copr=None):
         class F(FlaskForm):
             # also use id here, to be able to find out whether user
             # is updating a copr if so, we don't want to shout
@@ -268,7 +268,8 @@ class CoprFormFactory(object):
                 validators=[
                     wtforms.validators.Optional(),
                     wtforms.validators.NumberRange(min=0, max=60),
-                ])
+                ],
+                render_kw={'disabled': bool(copr and copr.persistent)})
 
             repos = wtforms.TextAreaField(
                 "External Repositories",
@@ -282,16 +283,61 @@ class CoprFormFactory(object):
                     UrlSrpmListValidator()],
                 filters=[StringListFilter()])
 
-            disable_createrepo = wtforms.BooleanField(default=False, false_values=FALSE_VALUES)
-            unlisted_on_hp = wtforms.BooleanField("Do not display this project on home page", default=False, false_values=FALSE_VALUES)
-            persistent = wtforms.BooleanField(default=False, false_values=FALSE_VALUES)
-            auto_prune = wtforms.BooleanField("If backend auto-prunning script should be run for this project", default=True, false_values=FALSE_VALUES)
-            use_bootstrap_container = wtforms.BooleanField("Enable use_bootstrap_container mock's feature (experimental)", default=False, false_values=FALSE_VALUES)
-            follow_fedora_branching = wtforms.BooleanField("If newly branched chroots should be automatically enabled and populated.", default=True, false_values=FALSE_VALUES)
+            disable_createrepo = wtforms.BooleanField(default=False,
+                    label="Create repositories manually",
+                    description="""When Fedora is branched from rawhide, the
+                    respective chroots for the new branch are automatically
+                    created for you (as soon as they are available) as rawhide
+                    chroot forks.""",
+                    false_values=FALSE_VALUES)
+
+            unlisted_on_hp = wtforms.BooleanField(
+                    "Project will not be listed on home page",
+                    default=False,
+                    false_values=FALSE_VALUES)
+
+            persistent = wtforms.BooleanField(
+                    "Protect project and its builds against deletion",
+                    description="""Project's builds and the project itself
+                    cannot be deleted by any means.  This option is set once and
+                    for all (this option can not be changed after project is
+                    created).""",
+                    render_kw={'disabled': bool(copr)},
+                    default=False, false_values=FALSE_VALUES)
+
+            auto_prune = wtforms.BooleanField(
+                    "Old builds will be deleted automatically",
+                    default=True, false_values=FALSE_VALUES,
+                    description="""Build will be deleted only if there is a
+                    newer build (with respect to package version) and it is
+                    older than 14 days""")
+
+            use_bootstrap_container = wtforms.BooleanField(
+                    "Enable mock's use_bootstrap_container experimental feature",
+                    description="""This will make the build slower but it has an
+                    advantage that the dnf _from_ the given chroot will be used
+                    to setup the chroot (otherwise host system dnf and rpm is
+                    used)""",
+                    default=False,
+                    false_values=FALSE_VALUES)
+
+            follow_fedora_branching = wtforms.BooleanField(
+                    "Follow Fedora branching",
+                    description="""When Fedora is branched from rawhide, the
+                    respective chroots for the new branch are automatically
+                    created for you (as soon as they are available) as rawhide
+                    chroot forks.""",
+                    default=True,
+                    false_values=FALSE_VALUES)
 
             # Deprecated, use `enable_net` instead
-            build_enable_net = wtforms.BooleanField(default=False, false_values=FALSE_VALUES)
-            enable_net = wtforms.BooleanField(default=False, false_values=FALSE_VALUES)
+            build_enable_net = wtforms.BooleanField(
+                    "Enable internet access during builds",
+                    default=False, false_values=FALSE_VALUES)
+
+            enable_net = wtforms.BooleanField(
+                    "Enable internet access during builds",
+                    default=False, false_values=FALSE_VALUES)
 
             @property
             def selected_chroots(self):
@@ -308,6 +354,12 @@ class CoprFormFactory(object):
                 if not self.validate_mock_chroots_not_empty():
                     self.errors["chroots"] = ["At least one chroot must be selected"]
                     return False
+
+                if self.persistent.data and self.delete_after_days.data:
+                    self.delete_after_days.errors.append(
+                        "'delete after' can not be combined with persistent")
+                    return False
+
                 return True
 
             def validate_mock_chroots_not_empty(self):

frontend/coprs_frontend/coprs/static/css/custom-styles.css
@@ -36,5 +36,5 @@ span.padding {
 }
 
 input.short-input-field {
-    width: 10em;
+    width: 6em;
 }

frontend/coprs_frontend/coprs/templates/_helpers.html
@@ -30,6 +30,34 @@
   {% endif %}
 {% endmacro %}
 
+{% macro render_checkbox_field(title, items) %}
+<div class="form-group">
+  <label class="col-sm-2 control-label" for="textInput-markup">
+    {{ title }}
+  </label>
+  <div class="col-sm-10">
+    {% for tuple in items %}
+      {% set checkbox = tuple[0] %}
+      {% set show     = tuple[1] if tuple|length > 1 else True %}
+      {% if show %}
+    <div class="checkbox">
+      <label>
+        {{ checkbox }}
+        {{ checkbox.label.text }}
+        {% if checkbox.description %}
+        <br>
+        <small class="text-muted pficon pficon-info">
+          {{ checkbox.description }}
+        </small>
+        {% endif %}
+      </label>
+    </div>
+      {% endif %}
+    {% endfor %}
+  </div>
+</div>
+{% endmacro %}
+
 {% macro render_pypi_python_versions_field(field) %}
   <div class="form-group {% if field.errors %}has-error{% endif %}">
     <label class="col-sm-2 control-label">

frontend/coprs_frontend/coprs/templates/coprs/_coprs_forms.html
@@ -1,4 +1,4 @@
-{% from "_helpers.html" import render_field, render_form_errors, copr_details_href, copr_url %}
+{% from "_helpers.html" import render_field, render_checkbox_field, render_form_errors, copr_details_href, copr_url %}
 
 {% macro copr_form(form, view, copr = None, username = None, group = None) %}
   {# if using for updating, we need to pass name to url_for, but otherwise we need to pass nothing #}
@@ -21,7 +21,7 @@
 
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h3 class="panel-title">1. Project information</h3>
+      <h3 class="panel-title">{{ counter('instructions') }}. Project information</h3>
     </div>
     <div class="panel-body">
 
@@ -44,15 +44,12 @@
 
     {{ render_field(form.homepage, label='Homepage', placeholder='Optional - project homepage') }}
     {{ render_field(form.contact, label='Contact', placeholder='Optional - email address or contact url', info="Use e-mail address or a link to your issue tracker. This information will be shown publicly.") }}
-    {{ render_field(form.delete_after_days,
-                    class="short-input-field",
-                    placeholder='Optional',
-                    info='Delete the project after the specfied period of time (empty = disabled)') }}
     </div>
   </div>
+
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h3 class="panel-title">2. Build options</h3>
+      <h3 class="panel-title">{{ counter('instructions')}}. Build options</h3>
     </div>
     <div class="panel-body">
 
@@ -112,72 +109,14 @@
       </div>
     </div>
 
-
-    <div class="form-group">
-      <label class="col-sm-2 control-label" for="textInput-markup">
-        Other Options
-      </label>
-      <div class="col-sm-10">
-        <div class="checkbox">
-          {{ form.disable_createrepo }} Create repositories manually
-          <br>
-          <small class="text-muted pficon pficon-info">
-            Repository meta data is normally refreshed after each build. If you want to do this manually, check the option above.
-          </small>
-        </div>
-        <div class="checkbox">
-          {{ form.build_enable_net }} Enable internet access during builds
-        </div>
-        {% if g.user.admin %}
-        <div class="checkbox">
-          {{ form.auto_prune }} Old builds will be deleted automatically
-          <small class="text-muted pficon pficon-info">
-            Build will be deleted only if there is a newer build (with respect to package version) and it is older than 14 days
-          </small>
-        </div>
-        {% endif %}
-        <div class="checkbox">
-          {{ form.unlisted_on_hp }} Project will not be listed on home page
-        </div>
-        {% if g.user.admin and not copr %} {# the following setting is available only for admins on creating a new project #}
-        <div class="checkbox">
-          {{ form.persistent }} Protect project and its builds against deletion
-          <br>
-          <small class="text-muted pficon pficon-info">
-            Project's builds and the project itself cannot be deleted by any means. This option is set once and for all.
-          </small>
-        </div>
-        {% endif %}
-        {% if g.user.admin and copr %} {# let an admin see the status of the 'Persistent builds' field but do not let him change the preset value #}
-        <div class="checkbox">
-          {{ form.persistent(disabled=True) }} Protect project and its builds against deletion
-          <br/><small class="text-muted pficon pficon-info">
-            This option cannot be altered after a project has been created.
-          </small>
-        </div>
-        {% endif %}
-        <div class="checkbox">
-          {{ form.use_bootstrap_container }} Enable mock's use_bootstrap_container experimental feature
-          <small class="text-muted pficon pficon-info">
-            This will make the build slower but it has an advantage that the dnf _from_ the given chroot will be used to setup the chroot (otherwise host system dnf and rpm is used)
-          </small>
-        </div>
-        <div class="checkbox">
-          {{ form.follow_fedora_branching }} Follow Fedora branching
-          <small class="text-muted pficon pficon-info">
-            When Fedora is branched from rawhide, the respective chroots for the new branch are automatically created for you (as soon as they are available) as rawhide chroot forks.
-          </small>
-        </div>
-      </div>
-    </div>
-
     </div>
   </div>
+
 
   {% if copr is none %}{# we're creating the copr, so display initial builds area #}
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h3 class="panel-title">3. (Optional) initial builds </h3>
+        <h3 class="panel-title">{{ counter('instructions') }}. (Optional) initial builds </h3>
     </div>
     <div class="panel-body">
 
@@ -185,6 +124,32 @@
     </div>
   </div>
   {% endif %}
+
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">{{ counter('instructions') }}. Other options</h3>
+    </div>
+    <div class="panel-body">
+
+    {{ render_checkbox_field(
+        "Booleans", [
+        [form.disable_createrepo],
+        [form.build_enable_net],
+        [form.auto_prune, g.user.admin],
+        [form.unlisted_on_hp],
+        [form.persistent, g.user.admin],
+        [form.use_bootstrap_container],
+        [form.follow_fedora_branching],
+    ])}}
+
+    {{ render_field(form.delete_after_days,
+            class="short-input-field",
+            placeholder='Optional',
+            info='Delete the project after the specfied period of time (empty = disabled)') }}
+
+    </div>
+  </div>
 
     <input class="btn btn-primary" type="submit" value="{% if copr %}Update{% else %}Create{% endif %}">
   </form>

frontend/coprs_frontend/coprs/templates/coprs/add.html
@@ -1,7 +1,7 @@
 {% extends "layout.html" %}
 {% block title %}Add a Project{% endblock %}
 {% block header %}Add a new Project{% endblock %}
-{% from "coprs/_coprs_forms.html" import copr_form %}
+{% from "coprs/_coprs_forms.html" import copr_form with context %}
 
 {% block breadcrumbs %}
 <ol class="breadcrumb">

frontend/coprs_frontend/coprs/templates/coprs/detail/settings/edit.html
@@ -1,5 +1,5 @@
 {% extends "coprs/detail/settings.html" %}
-{% from "coprs/_coprs_forms.html" import copr_form %}
+{% from "coprs/_coprs_forms.html" import copr_form with context %}
 
 {% set selected_monitor_tab = "edit" %}
 {%block settings_breadcrumb %}Project Details{% endblock %}

frontend/coprs_frontend/coprs/templates/coprs/group_add.html
@@ -1,7 +1,7 @@
 {% extends "layout.html" %}
 {% block title %}Add a new Group Project{% endblock %}
 {% block header %}Add a new Group Project{% endblock %}
-{% from "coprs/_coprs_forms.html" import copr_form %}
+{% from "coprs/_coprs_forms.html" import copr_form with context %}
 
 {% block breadcrumbs %}
 <ol class="breadcrumb">

frontend/coprs_frontend/coprs/views/coprs_ns/coprs_general.py
@@ -421,7 +421,7 @@ def copr_integrations_update(copr):
 def render_copr_edit(copr, form, view):
     if not form:
         form = forms.CoprFormFactory.create_form_cls(
-            copr.mock_chroots)(obj=copr)
+            copr.mock_chroots, copr=copr)(obj=copr)
     return flask.render_template(
         "coprs/detail/settings/edit.html",
         copr=copr, form=form, view=view)

commit 33175fae2b68af79bb17833e6390b885824b237a

[frontend] correctly describe "Create repositories manually"
This is C&P bug caused by 3e7c629460c86.

Fixes: #739, PR#740

frontend/coprs_frontend/coprs/forms.py
@@ -285,10 +285,9 @@ class CoprFormFactory(object):
 
             disable_createrepo = wtforms.BooleanField(default=False,
                     label="Create repositories manually",
-                    description="""When Fedora is branched from rawhide, the
-                    respective chroots for the new branch are automatically
-                    created for you (as soon as they are available) as rawhide
-                    chroot forks.""",
+                    description="""Repository meta data is normally refreshed
+                    after each build.  If you want to do this manually, turn
+                    this option on.""",
                     false_values=FALSE_VALUES)
 
             unlisted_on_hp = wtforms.BooleanField(