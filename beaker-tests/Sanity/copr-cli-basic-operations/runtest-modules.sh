8519caec2735f0292e30768b855c1bcbc4d5530e
Jakub Kadlcik <frostyx@email.cz>
2019-07-26 10:02:22 +0200

    beaker-tests-sanity: fix the way of checking whether a module is visible on the system
    The output of `dnf module list` have probably changed because now the
    test doesn't make any sense. There is no version number on this line.

--- beaker-tests/Sanity/copr-cli-basic-operations/runtest-modules.sh
+++ beaker-tests/Sanity/copr-cli-basic-operations/runtest-modules.sh
@@ -189,7 +189,8 @@ rlJournalStart
         # https://github.com/rpm-software-management/dnf-plugins-core/pull/214
         rlRun "curl $FRONTEND_URL/coprs/$USER/module-testmodule-beakertest-$DATE/module_repo/fedora-$FEDORA_VERSION/testmodule-beakertest-$DATE.repo > /etc/yum.repos.d/testmodule.repo"
 
-        rlAssertEquals "Module should be visible in the system" `dnf module list |grep testmodule |grep beakertest |grep $DATE |grep -v "Copr modules repo" |wc -l` 1
+        rlAssertEquals "Module should be visible in the system" `dnf module list |grep testmodule |grep beakertest |grep -v "Copr modules repo" |wc -l` 1
+        rlAssertEquals "Module should be available in the correct version" `dnf module info testmodule:beakertest |grep Version |grep $DATE |wc -l` 1
         rlRun "dnf module enable testmodule:beakertest"
         rlRun "dnf -y module install testmodule/default"
         rlRun "rpm -q mksh"
