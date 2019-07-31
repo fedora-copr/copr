e510fa87e3559465f23f3500ad1b7fe72571e30b
Pavel Raiskup <praiskup@redhat.com>
2019-07-31 14:37:24 +0200

    beaker-tests: respect $COPR_CLEANUP in runtest-epel.sh

--- beaker-tests/Sanity/copr-cli-basic-operations/runtest-epel.sh
+++ beaker-tests/Sanity/copr-cli-basic-operations/runtest-epel.sh
@@ -17,10 +17,10 @@ rlJournalStart
     rlPhaseStartTest
         rlRun "copr-cli create ${NAME_PREFIX}TestEpel --chroot epel-6-x86_64 --chroot epel-7-x86_64" 0
         rlRun "copr-cli build ${NAME_PREFIX}TestEpel $HELLO" 0
-        rlRun "copr-cli delete ${NAME_PREFIX}TestEpel"
     rlPhaseEnd
 
     rlPhaseStartCleanup
+        cleanProject "${NAME_PREFIX}TestEpel"
     rlPhaseEnd
 rlJournalPrintText
 rlJournalEnd
