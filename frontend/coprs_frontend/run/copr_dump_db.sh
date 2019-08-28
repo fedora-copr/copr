5363335ea30c95aad6bc4738c9828382e01a7906
Dominik Turecek <dturecek@redhat.com>
2019-08-28 13:07:20 +0200

    frontend: add queries to the end of public dump to fill private tables
    Fixes: #912

--- frontend/coprs_frontend/run/copr_dump_db.sh
+++ frontend/coprs_frontend/run/copr_dump_db.sh
@@ -9,7 +9,15 @@ fi
 
 dump_dir=$1
 
-runuser -c 'pg_dump --exclude-table-data "*_private" coprdb' - postgres \
-    | runuser -c "gzip > $dump_dir/copr_db-$(date '+%Y-%m-%d_%H-%M').gz" - copr-fe \
+(
+    runuser -c 'pg_dump --exclude-table-data "*_private" coprdb' - postgres
+    cat <<'EOF'
+INSERT INTO public.user_private (mail, api_login, api_token, api_token_expiration, user_id)
+SELECT '', '', '', '2000-01-01', id FROM public."user";
+
+INSERT INTO public.copr_private (copr_id)
+SELECT id FROM public.copr;
+EOF
+) | runuser -c "gzip > $dump_dir/copr_db-$(date '+%Y-%m-%d_%H-%M').gz" - copr-fe
 
 find "$dump_dir" -name 'copr_db-*.gz' -mtime +1 -delete
