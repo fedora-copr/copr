43e3f7ccf8826bf052ab730718a07208482889d5
Tomas Hrnciar <thrnciar@reedhat.com>
2019-08-29 11:06:02 +0200

    frontend: hide unlisted projects from homepage in RSS
    This commit removes unlisted project at homepage in RSS and
    adds new RSS route /rss/all/ to show also unlisted projects.
    
    Merges: #968

--- frontend/coprs_frontend/coprs/views/rss_ns/rss_general.py
+++ frontend/coprs_frontend/coprs/views/rss_ns/rss_general.py
@@ -8,9 +8,8 @@ from flask import render_template, Response
 from coprs import models
 
 
-@rss_ns.route("/", defaults={"limit": 200})
-@rss_ns.route("/<int:limit>/")
-def rss(limit=200):
+@rss_ns.route("/all", defaults={"limit": 200})
+def rss_all(limit=200):
     """
     Simple route that returns all projects
     name, description, link to selected project
@@ -21,5 +20,18 @@ def rss(limit=200):
     coprs = CoprsLogic.get_all().order_by(models.Copr.id.desc()).limit(limit)
 
     answer = render_template("rss/rss.xml", coprs=coprs)
+    return Response(answer, mimetype="text/xml")
 
+
+@rss_ns.route("/", defaults={"limit": 200})
+@rss_ns.route("/<int:limit>/")
+def rss(limit=200):
+    """
+    Simple route that returns all projects
+    name, description, link to selected project
+    as rss feed except projects hidden from homepage
+    """
+
+    coprs = CoprsLogic.get_multiple(include_unlisted_on_hp=False).order_by(models.Copr.id.desc()).limit(limit)
+    answer = render_template("rss/rss.xml", coprs=coprs)
     return Response(answer, mimetype="text/xml")
