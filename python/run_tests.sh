2871caf16270ab54859802835b4ba6f54ba36ead
Pavel Raiskup <praiskup@redhat.com>
2019-09-02 14:14:40 +0200

    python: fix run_tests.sh wrapper

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#python -B -m pytest --cov-report term-missing --cov ./copr_cli/ tests $@
 
-PYTHONPATH=.:$PYTHONPATH python -B -m pytest --cov-report term-missing --cov ./copr/client_v2/ -s $@
+absdir="$(dirname "$(readlink -f "$0")")"
+export PYTHONPATH="$absdir"
+
+python3 -B -m pytest --cov-report term-missing copr/test "$@"
