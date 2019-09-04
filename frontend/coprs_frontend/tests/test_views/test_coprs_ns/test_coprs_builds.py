fedora-copr/copr

commit 8e8867b5a549f88119a913c20606bc19c6c5e988

frontend: allow admin to edit package
fixes #827

frontend/coprs_frontend/coprs/models.py
@@ -115,21 +115,21 @@ class User(db.Model, helpers.Serializer):
         """
         Determine if this user can build in the given copr.
         """
-        can_build = False
+        if copr.user.admin:
+            return True
         if copr.user_id == self.id:
-            can_build = True
+            return True
         if (self.permissions_for_copr(copr) and
                 self.permissions_for_copr(copr).copr_builder ==
                 helpers.PermissionEnum("approved")):
-
-            can_build = True
+            return True
 
         # a bit dirty code, here we access flask.session object
         if copr.group is not None and \
                 copr.group.fas_name in self.user_teams:
             return True
 
-        return can_build
+        return False
 
     @property
     def user_teams(self):

frontend/coprs_frontend/tests/test_views/test_coprs_ns/test_coprs_builds.py
@@ -59,7 +59,7 @@ class TestCoprAddBuild(CoprsTestCase):
                                                          f_coprs,
                                                          f_copr_permissions,
                                                          f_db):
-
+        self.u1.admin = False
         self.db.session.add_all([self.u1, self.c1])
         self.test_client.post("/coprs/{0}/{1}/new_build/"
                               .format(self.u1.name, self.c1.name),
@@ -111,6 +111,7 @@ class TestCoprCancelBuild(CoprsTestCase):
         for bc in self.b1_bc:
             bc.status = StatusEnum("pending")
             bc.ended_on = None
+        self.u1.admin = False
         self.db.session.add_all(self.b1_bc)
         self.db.session.add_all([self.u1, self.c1, self.b1])
         self.test_client.post("/coprs/{0}/{1}/cancel_build/{2}/"
@@ -280,7 +281,7 @@ class TestCoprRepeatBuild(CoprsTestCase):
                                                           f_coprs,
                                                           f_mock_chroots,
                                                           f_builds, f_db):
-
+        self.u1.admin = False
         self.db.session.add_all([self.u1, self.c1, self.b1])
         r = self.test_client.post(
             "/coprs/{0}/{1}/repeat_build/{2}/"

frontend/coprs_frontend/tests/test_api/test_build_r.py
@@ -210,6 +210,7 @@ class TestBuildResource(CoprsTestCase):
             "srpm_url": "http://example.com/mypkg.src.rpm",
             "chroots": chroot_name_list
         }
+        self.u1.admin = False
         self.db.session.commit()
         r0 = self.request_rest_api_with_auth(
             "/api_2/builds",
@@ -255,6 +256,7 @@ class TestBuildResource(CoprsTestCase):
         }
         login = self.u2.api_login
         token = self.u2.api_token
+        self.u1.admin = False
         self.db.session.commit()
         r0 = self.request_rest_api_with_auth(
             "/api_2/builds",
@@ -458,6 +460,8 @@ class TestBuildResource(CoprsTestCase):
             bc.status = StatusEnum("pending")
             bc.ended_on = None
 
+        self.u1.admin = False
+        
         self.db.session.add_all(self.b1_bc)
         self.db.session.add(self.b1)
 

commit f4b119283841a29a54b8ff43b8068cebd7a79682

frontend: admin permission check in 'can_build_in()'
Commit 8e8867b5a caused that we didn't compare the admin permissions of
the logged user, but the owner of copr we tried to build against.

Fix test-cases which didn't expect that the requesting user may be
admin, and add new test-case for the bug itself (that normal user can
not build in admin's project).

Fixes: #970

frontend/coprs_frontend/coprs/models.py
@@ -115,7 +115,7 @@ class User(db.Model, helpers.Serializer):
         """
         Determine if this user can build in the given copr.
         """
-        if copr.user.admin:
+        if self.admin:
             return True
         if copr.group:
             if self.can_build_in_group(copr.group):

frontend/coprs_frontend/tests/test_views/test_coprs_ns/test_coprs_builds.py
@@ -46,13 +46,27 @@ class TestCoprAddBuild(CoprsTestCase):
     def test_copr_not_yet_allowed_user_cant_add_build(self, f_users, f_coprs,
                                                       f_copr_permissions, f_db):
 
-        self.db.session.add_all([self.u2, self.c3])
+        self.u1.admin = False
+        self.db.session.add_all([self.u1, self.u2, self.c3])
+
         self.test_client.post("/coprs/{0}/{1}/new_build/"
                               .format(self.u2.name, self.c3.name),
                               data={"pkgs": "http://example.com/testing.src.rpm"},
                               follow_redirects=True)
 
         assert not self.models.Build.query.first()
+
+    @TransactionDecorator("u2")
+    def test_copr_user_cant_add_build_to_admin_project(self, f_users, f_coprs,
+                                                       f_copr_permissions, f_db):
+        """ test for issue#970 """
+        self.db.session.add_all([self.u1, self.c1])
+        self.test_client.post("/coprs/{0}/{1}/new_build/"
+                              .format(self.u1.name, self.c1.name),
+                              data={"pkgs": "http://example.com/testing.src.rpm"},
+                              follow_redirects=True)
+        assert not self.models.Build.query.first()
+
 
     @TransactionDecorator("u3")
     def test_copr_user_without_permission_cant_add_build(self, f_users,

frontend/coprs_frontend/tests/test_views/test_coprs_ns/test_coprs_general.py
@@ -261,8 +261,8 @@ class TestCoprDetail(CoprsTestCase):
     @TransactionDecorator("u1")
     def test_copr_detail_doesnt_allow_non_submitter_to_cancel_build(
             self, f_users, f_coprs, f_mock_chroots, f_builds, f_db):
-
-        self.db.session.add_all([self.u2, self.c2])
+        self.u1.admin = False
+        self.db.session.add_all([self.u1, self.u2, self.c2])
         r = self.test_client.get(
             "/coprs/{0}/{1}/build/{2}/".format(self.u2.name, self.c2.name, self.c2.builds[0].id))
         assert b"/cancel_build/" not in r.data