2474998a8d5575b6235eee34876fd8ead8c027d6
Pavel Raiskup <praiskup@redhat.com>
2019-09-10 20:41:05 +0200

    frontend: fixup migration order
    Two pull-requests (#938 and #853) were merged with the same
    "down_revision" tag.

--- frontend/coprs_frontend/alembic/schema/versions/0dbdd06fb850_multilib_knob_on_copr_public.py
+++ frontend/coprs_frontend/alembic/schema/versions/0dbdd06fb850_multilib_knob_on_copr_public.py
@@ -11,7 +11,7 @@ from alembic import op
 
 
 revision = '0dbdd06fb850'
-down_revision = '12abab545d7a'
+down_revision = '6800e08934eb'
 
 def upgrade():
     op.add_column('copr', sa.Column('multilib', sa.Boolean(), server_default='0', nullable=False))
