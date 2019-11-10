d7837bfbfe032075d7c69997c67ab0f0b9260666
Jakub Kadlcik <frostyx@email.cz>
2019-11-10 22:21:07 +0100

    docker: fix already running httpd issue on distgit
    I was trying to hackishly fix this in 6ae31277, but it probably won't
    help, because `pidof httpd` is none.
    
        [root@distgit /]# pidof httpd
        [root@distgit /]# /usr/sbin/httpd -DFOREGROUND
        httpd (pid 35) already running
    
    The mentioned PID is of the following process
    
        /usr/libexec/git-core/git-daemon --base-path=/var/lib/dist-git/git \
            --export-all --user-path=public_git --syslog --inetd --verbose
    
    Which is started by our supervisord. The question is, do we really need
    it? I don't think so, since we can just remove it and the docker compose
    environment works just fine.
    
    Merges: #1098

--- docker/distgit/files/usr/bin/run.sh
+++ docker/distgit/files/usr/bin/run.sh
@@ -9,11 +9,4 @@ cat <<-EOF
   echo "#########################################################"
 EOF
 
-# We have a recurring problem with httpd, which manifests itself
-# in the following way
-# 1. httpd is running, while supervisorctl thinks it failed
-# 2. It is not accessible e.g. via curl and returns "connection refused"
-# 3. Killing httpd and starting it again fixes the issue
-kill -9 `pidof httpd` && supervisorctl start httpd
-
 /bin/bash
