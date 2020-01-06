fedora-copr/copr

commit a57f7e3c1b6cc51907834f2ea2c1128b7844f953

frontend: cache the recent task queries
So as part of this commit, initialize the flask_cache mechanism, using
the already running redis database (we use it for session management).
The 'cache' object logically belongs to __init__.py, though it needs to
be placed on a pretty weird place (that's because there are
bi-directional dependencies among our modules, this is a problem but to
be solved in a different PR).

Also, instead of caching get_recent_tasks() directly, I created
get_recent_task_ids() which is safe for caching.  That's because the
contents is just a list of integers, not sqlalchemy objects.

The problem of caching sqlalchemy objects is that they allow
lazy-loading later in templates, and if loaded from cache - when the
object isn't bound to any session - the lazy load attempt would fail.
Therefore we cache just the IDs, and before passing the objects down to
the template we perform another (but way cheaper) db query.

frontend/coprs_frontend/coprs/logic/builds_logic.py
@@ -19,6 +19,7 @@ from sqlalchemy.exc import IntegrityError
 
 from copr_common.enums import FailTypeEnum, StatusEnum
 from coprs import app
+from coprs import cache
 from coprs import db
 from coprs import models
 from coprs import helpers
@@ -66,7 +67,8 @@ class BuildsLogic(object):
         return result
 
     @classmethod
-    def get_recent_tasks(cls, user=None, limit=100, period_days=2):
+    @cache.memoize(timeout=2*60)
+    def get_recent_task_ids(cls, user=None, limit=100, period_days=2):
         query_args = (
             models.BuildChroot.build_id,
             func.max(models.BuildChroot.ended_on).label('max_ended_on'),
@@ -94,6 +96,12 @@ class BuildsLogic(object):
         subquery = subquery.order_by(desc('max_ended_on')).limit(limit).subquery()
 
         query = models.Build.query.join(subquery, subquery.c.build_id == models.Build.id)
+        return [i.id for i in query.all()]
+
+    @classmethod
+    def get_recent_tasks(cls, *args, **kwargs):
+        task_ids = cls.get_recent_task_ids(*args, **kwargs)
+        query = models.Build.query.filter(models.Build.id.in_(task_ids))
         return list(query.all())
 
     @classmethod

frontend/copr-frontend.spec
@@ -78,6 +78,7 @@ BuildRequires: python3-copr-common > 0.4
 BuildRequires: python3-dateutil
 BuildRequires: python3-decorator
 BuildRequires: python3-flask
+BuildRequires: python3-flask-cache
 BuildRequires: python3-flask-openid
 BuildRequires: python3-flask-restful
 BuildRequires: python3-flask-script
@@ -131,6 +132,7 @@ Requires: python3-blinker
 Requires: python3-copr-common > 0.4
 Requires: python3-dateutil
 Requires: python3-flask
+Requires: python3-flask-cache
 Requires: python3-flask-openid
 Requires: python3-flask-restful
 Requires: python3-flask-script

frontend/coprs_frontend/coprs/__init__.py
@@ -5,6 +5,7 @@ import flask
 
 from flask_sqlalchemy import SQLAlchemy
 from contextlib import contextmanager
+from flask_cache import Cache
 from flask_openid import OpenID
 from flask_whooshee import Whooshee
 from openid_teams.teams import TeamsResponse
@@ -56,6 +57,15 @@ import coprs.whoosheers
 from coprs.helpers import RedisConnectionProvider
 rcp = RedisConnectionProvider(config=app.config)
 app.session_interface = RedisSessionInterface(rcp.get_connection())
+
+cache_rcp = RedisConnectionProvider(config=app.config, db=1)
+cache = Cache(app, config={
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_DB': 1,    # we use 0 for sessions
+    'CACHE_KEY_PREFIX': 'copr_cache_',
+    'CACHE_REDIS_HOST': cache_rcp.host,
+    'CACHE_REDIS_PORT': cache_rcp.port,
+})
 
 from coprs.views import admin_ns
 from coprs.views.admin_ns import admin_general

frontend/coprs_frontend/coprs/helpers.py
@@ -312,12 +312,13 @@ class Serializer(object):
 
 
 class RedisConnectionProvider(object):
-    def __init__(self, config):
+    def __init__(self, config, db=0):
         self.host = config.get("REDIS_HOST", "127.0.0.1")
         self.port = int(config.get("REDIS_PORT", "6379"))
+        self.db = db
 
     def get_connection(self):
-        return StrictRedis(host=self.host, port=self.port)
+        return StrictRedis(host=self.host, port=self.port, db=self.db)
 
 
 def get_redis_connection():

commit 45fc55a1ec182dca33db9ff65ea75eb0005b3fa8

frontend: sort recent tasks after caching again
Fixes bug caused by a57f7e3c1b6cc51907834f2ea2c1128b7844f953

frontend/coprs_frontend/coprs/logic/builds_logic.py
@@ -102,7 +102,7 @@ class BuildsLogic(object):
     def get_recent_tasks(cls, *args, **kwargs):
         task_ids = cls.get_recent_task_ids(*args, **kwargs)
         query = models.Build.query.filter(models.Build.id.in_(task_ids))
-        return list(query.all())
+        return sorted(query.all(), key=lambda o: task_ids.index(o.id))
 
     @classmethod
     def get_running_tasks_by_time(cls, start, end):