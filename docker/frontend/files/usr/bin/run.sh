76b0148ab420ec5c4afb30c8908a050738f85866
Jakub Kadlcik <frostyx@email.cz>
2020-01-07 18:55:29 +0100

    [docker] remove supervisord from builder container
    I am leaving the `/usr/bin/run.sh` script instead of running
    httpd directly in Dockerfile `CMD` because we need to run
    database migrations from somewhere.

--- docker/frontend/files/usr/bin/run.sh
+++ docker/frontend/files/usr/bin/run.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
-export LANG=en_US.UTF-8
-
-/usr/bin/supervisord -c /etc/supervisord.conf
-
-chown -R copr-fe:copr-fe /var/log/copr-frontend
-chown -R copr-fe:copr-fe /usr/share/copr
-
 cd /usr/share/copr/coprs_frontend/ && sudo -u copr-fe copr-frontend create-db --alembic alembic.ini
 sudo -u copr-fe copr-frontend create-chroot \
     $(ls /etc/mock/{fedora,epel}-*-{i386,x86_64}.cfg |xargs -I{} -n1 basename {} .cfg)
 
-# selinux: make data dir writeable for httpd
-# TODO: probly correct solution is to uncomment first four lines in
-# coprs_frontend/config/copr.conf so that data are stored under /var/lib
-# and not under /usr/share/copr. copr-selinux does not account for storing
-# data under /usr/share/copr/. Discuss this with peers.
-chcon -R -t httpd_sys_rw_content_t /usr/share/copr/data
-
-
-
-echo "#########################################################"
-echo "###   Your development instance of Copr Frontend      ###"
-echo "###   is now running at: http://localhost:5000        ###"
-echo "#########################################################"
-
-# Workaround
-setenforce 0
-
-supervisorctl restart httpd
-
-/bin/bash
+/usr/sbin/httpd -DFOREGROUND
