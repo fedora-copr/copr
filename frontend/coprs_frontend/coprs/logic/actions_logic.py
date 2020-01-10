fedora-copr/copr

commit b18cf83599a988fab84c72bf13757f4911b8516b

frontend: adds 24h and 90d graphs for actions
Fixes: #1103

frontend/coprs_frontend/coprs/logic/actions_logic.py
@@ -7,6 +7,8 @@ from coprs import models
 from coprs import helpers
 from coprs import exceptions
 
+from .helpers import get_graph_parameters
+from sqlalchemy import and_, or_
 
 class ActionsLogic(object):
 
@@ -62,11 +64,13 @@ class ActionsLogic(object):
         Updates result, message and ended_on parameters.
         """
 
-        for attr in ["result", "message", "ended_on"]:
+        for attr in ["result", "message"]:
             value = upd_dict.get(attr, None)
             if value:
                 setattr(action, attr, value)
 
+            if attr == "result" and value in [BackendResultEnum("success"), BackendResultEnum("waiting")]:
+                setattr(action, "ended_on", time.time())
         db.session.add(action)
 
     @classmethod
@@ -314,3 +318,89 @@ class ActionsLogic(object):
             created_on=int(time.time())
         )
         db.session.add(action)
+
+    @classmethod
+    def cache_action_graph_data(cls, type, time, waiting, success, failure):
+        result = models.ActionsStatistics.query\
+                .filter(models.ActionsStatistics.stat_type == type)\
+                .filter(models.ActionsStatistics.time == time).first()
+        if result:
+            return
+
+        try:
+            cached_data = models.ActionsStatistics(
+                time = time,
+                stat_type = type,
+                waiting = waiting,
+                success = success,
+                failed = failure
+            )
+            db.session.add(cached_data)
+            db.session.commit()
+        except IntegrityError: # other process already calculated the graph data and cached it
+            db.session.rollback()
+
+    @classmethod
+    def get_actions_bucket(cls, start, end, actionType):
+        if actionType == 0:
+            # used for getting data for "processed" line of action graphs
+            result = models.Action.query\
+                .filter(and_(
+                    models.Action.created_on <= end,
+                    or_(
+                        models.Action.ended_on > start,
+                        models.Action.ended_on == None
+                    )))\
+                .count()
+            return result
+
+        else:
+            # used to getting data for "successed and failure" line of action graphs
+            result = models.Action.query\
+                .filter(models.Action.ended_on <= end)\
+                .filter(models.Action.ended_on > start)\
+                .filter(models.Action.result == actionType)\
+                .count()
+            return result
+
+    @classmethod
+    def get_cached_action_data(cls, params):
+        data = {
+            "waiting": [],
+            "success": [],
+            "failure": [],
+        }
+        result = models.ActionsStatistics.query\
+            .filter(models.ActionsStatistics.stat_type == params["type"])\
+            .filter(models.ActionsStatistics.time >= params["start"])\
+            .filter(models.ActionsStatistics.time <= params["end"])\
+            .order_by(models.ActionsStatistics.time)
+        for row in result:
+            data["waiting"].append(row.waiting)
+            data["success"].append(row.success)
+            data["failure"].append(row.failed)
+
+        return data
+
+    @classmethod
+    def get_action_graph_data(cls, type):
+        data = [["processed"], ["success"], ["failure"], ["time"] ]
+        params = get_graph_parameters(type)
+        cached_data = cls.get_cached_action_data(params)
+        for actionType in ["waiting", "success", "failure"]:
+            data[BackendResultEnum(actionType)].extend(cached_data[actionType])
+        for i in range(len(data[0]) - 1, params["steps"]):
+            step_start = params["start"] + i * params["step"]
+            step_end = step_start + params["step"]
+            waiting = cls.get_actions_bucket(step_start, step_end, BackendResultEnum("waiting"))
+            success = cls.get_actions_bucket(step_start, step_end, BackendResultEnum("success"))
+            failure = cls.get_actions_bucket(step_start, step_end, BackendResultEnum("failure"))
+            data[0].append(waiting)
+            data[1].append(success)
+            data[2].append(failure)
+            cls.cache_action_graph_data(type, time=step_start, waiting=waiting, success=success, failure=failure)
+
+        for i in range(params["start"], params["end"], params["step"]):
+            data[3].append(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(i)))
+
+        return data

backend/backend/actions.py
@@ -582,7 +582,6 @@ class ActionWorkerManager(WorkerManager):
         result = Munch()
         result.id = int(task_id)
         result.result = int(task_info['status'])
-        result.ended_on = time.time()
 
         try:
             self.frontend_client.update({"actions": [result]})

frontend/coprs_frontend/alembic/schema/versions/a8ef299dcac8_add_indexes_to_actions.py
@@ -1,0 +1,22 @@
+"""
+add indexes to actions
+
+Revision ID: a8ef299dcac8
+Revises: cb928c34d36c
+Create Date: 2020-01-06 14:04:30.362083
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+revision = 'a8ef299dcac8'
+down_revision = 'cb928c34d36c'
+
+def upgrade():
+    op.create_index(op.f('ix_action_created_on'), 'action', ['created_on'], unique=False)
+    op.create_index(op.f('ix_action_ended_on'), 'action', ['ended_on'], unique=False)
+
+def downgrade():
+    op.drop_index(op.f('ix_action_ended_on'), table_name='action')
+    op.drop_index(op.f('ix_action_created_on'), table_name='action')

frontend/coprs_frontend/alembic/schema/versions/cb928c34d36c_add_actionsstatistics_table.py
@@ -1,0 +1,27 @@
+"""
+add ActionsStatistics table
+
+Revision ID: cb928c34d36c
+Revises: 2db1d0557b06
+Create Date: 2019-12-06 09:33:12.210295
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+
+revision = 'cb928c34d36c'
+down_revision = '2db1d0557b06'
+
+def upgrade():
+    op.create_table('actions_statistics',
+    sa.Column('time', sa.Integer(), nullable=False),
+    sa.Column('stat_type', sa.Text(), nullable=False),
+    sa.Column('waiting', sa.Integer(), nullable=True),
+    sa.Column('success', sa.Integer(), nullable=True),
+    sa.Column('failed', sa.Integer(), nullable=True),
+    sa.PrimaryKeyConstraint('time', 'stat_type')
+    )
+
+def downgrade():
+    op.drop_table('actions_statistics')

frontend/coprs_frontend/coprs/logic/builds_logic.py
@@ -34,6 +34,7 @@ from coprs.models import BuildChroot
 from .coprs_logic import MockChrootsLogic
 from coprs.logic.packages_logic import PackagesLogic
 
+from .helpers import get_graph_parameters
 log = app.logger
 
 
@@ -190,7 +191,7 @@ class BuildsLogic(object):
     @classmethod
     def get_task_graph_data(cls, type):
         data = [["pending"], ["running"], ["avg running"], ["time"]]
-        params = cls.get_graph_parameters(type)
+        params = get_graph_parameters(type)
         cached_data = cls.get_cached_graph_data(params)
         data[0].extend(cached_data["pending"])
         data[1].extend(cached_data["running"])
@@ -218,7 +219,7 @@ class BuildsLogic(object):
     @classmethod
     def get_small_graph_data(cls, type):
         data = [[""]]
-        params = cls.get_graph_parameters(type)
+        params = get_graph_parameters(type)
         cached_data = cls.get_cached_graph_data(params)
         data[0].extend(cached_data["running"])
 
@@ -250,33 +251,6 @@ class BuildsLogic(object):
             db.session.commit()
         except IntegrityError: # other process already calculated the graph data and cached it
             db.session.rollback()
-
-    @classmethod
-    def get_graph_parameters(cls, type):
-        if type is "10min":
-            # 24 hours with 10 minute intervals
-            step = 600
-            steps = 144
-        elif type is "30min":
-            # 24 hours with 30 minute intervals
-            step = 1800
-            steps = 48
-        elif type is "24h":
-            # 90 days with 24 hour intervals
-            step = 86400
-            steps = 90
-
-        end = int(time.time())
-        end = end - (end % step) # align graph interval to a multiple of step
-        start = end - (steps * step)
-
-        return {
-            "type": type,
-            "step": step,
-            "steps": steps,
-            "start": start,
-            "end": end,
-        }
 
     @classmethod
     def get_build_importing_queue(cls, background=None):

frontend/coprs_frontend/coprs/logic/helpers.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import time
 
 def slice_query(query, limit=100, offset=0):
     """
@@ -8,3 +9,29 @@ def slice_query(query, limit=100, offset=0):
     :rtype: Query
     """
     return query.limit(limit).offset(offset)
+
+def get_graph_parameters(type):
+    if type is "10min":
+        # 24 hours with 10 minute intervals
+        step = 600
+        steps = 144
+    elif type is "30min":
+        # 24 hours with 30 minute intervals
+        step = 1800
+        steps = 48
+    elif type is "24h":
+        # 90 days with 24 hour intervals
+        step = 86400
+        steps = 90
+
+    end = int(time.time())
+    end = end - (end % step) # align graph interval to a multiple of step
+    start = end - (steps * step)
+
+    return {
+        "type": type,
+        "step": step,
+        "steps": steps,
+        "start": start,
+        "end": end,
+    }

frontend/coprs_frontend/coprs/models.py
@@ -1477,9 +1477,9 @@ class Action(db.Model, helpers.Serializer):
     # optional message from the backend/whatever
     message = db.Column(db.Text)
     # time created as returned by int(time.time())
-    created_on = db.Column(db.Integer)
+    created_on = db.Column(db.Integer, index=True)
     # time ended as returned by int(time.time())
-    ended_on = db.Column(db.Integer)
+    ended_on = db.Column(db.Integer, index=True)
 
     def __str__(self):
         return self.__unicode__()
@@ -1657,3 +1657,10 @@ class BuildsStatistics(db.Model):
     stat_type = db.Column(db.Text, primary_key=True)
     running = db.Column(db.Integer)
     pending = db.Column(db.Integer)
+
+class ActionsStatistics(db.Model):
+    time = db.Column(db.Integer, primary_key=True)
+    stat_type = db.Column(db.Text, primary_key=True)
+    waiting = db.Column(db.Integer)
+    success = db.Column(db.Integer)
+    failed = db.Column(db.Integer)

frontend/coprs_frontend/coprs/static/js/graphs.js
@@ -19,7 +19,7 @@ function graphConfig() {
         },
         color: {pattern: colorPattern},
         data: {
-            hide: ['avg running'],
+            hide: ['avg running', 'success'],
             type: 'line',
             x: 'time',
             xFormat: '%Y-%m-%d %H:%M:%S'

commit d00145c3eb0a7ca261c3277211eddcdfd20d4741

frontend: fix testsuite broken by b18cf83599a988fa
In /backend/update/ route we need to set ended_on attribute for
{success, failure} results, not {success, waiting}.

Relates: #803

frontend/coprs_frontend/coprs/logic/actions_logic.py
@@ -69,8 +69,9 @@ class ActionsLogic(object):
             if value:
                 setattr(action, attr, value)
 
-            if attr == "result" and value in [BackendResultEnum("success"), BackendResultEnum("waiting")]:
-                setattr(action, "ended_on", time.time())
+        if upd_dict.get('result', None) in [BackendResultEnum("success"),
+                                            BackendResultEnum("failure")]:
+            action.ended_on = time.time()
         db.session.add(action)
 
     @classmethod

frontend/coprs_frontend/tests/test_views/test_backend_ns/test_backend_general.py
@@ -1,5 +1,7 @@
 import json
 
+from unittest import mock
+
 from copr_common.enums import BackendResultEnum, StatusEnum
 from tests.coprs_test_case import CoprsTestCase, new_app_context
 from coprs.logic.builds_logic import BuildsLogic
@@ -300,20 +302,17 @@ class TestUpdateActions(CoprsTestCase):
     {
       "id": 1,
       "result": 1,
-      "message": null,
-      "ended_on": 1390866440
+      "message": null
     },
     {
       "id": 2,
       "result": 2,
-      "message": "problem!",
-      "ended_on": 1390866440
+      "message": "problem!"
     },
     {
       "id": 100,
       "result": 123,
-      "message": "wheeeee!",
-      "ended_on": 1390866440
+      "message": "wheeeee!"
     }
   ]
 }"""
@@ -332,9 +331,12 @@ class TestUpdateActions(CoprsTestCase):
         assert updated.message == "no problem"
         assert updated.ended_on == 1390866440
 
-    def test_update_more_existent_and_non_existent_actions(self, f_users,
-                                                          f_coprs, f_actions,
-                                                          f_db):
+
+    @mock.patch('coprs.logic.actions_logic.time.time')
+    def test_update_more_existent_and_non_existent_actions(self, mc_time, f_users,
+                                                           f_coprs, f_actions,
+                                                           f_db):
+        mc_time.return_value = 1390866440
         r = self.tc.post("/backend/update/",
                          content_type="application/json",
                          headers=self.auth_header,