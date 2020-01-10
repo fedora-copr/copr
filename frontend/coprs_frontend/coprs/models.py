fedora-copr/copr

commit a34ad40c260e8e61a99d00c74783755731de0ba7

frontend: cache Build.status at runtime
Don't re-calculate Build.status from BuildChroot statuses if we already
did that.  Rather micro optimization.

frontend/coprs_frontend/coprs/models.py
@@ -880,6 +880,9 @@ class Build(db.Model, helpers.Serializer):
     # the info that the build was resubmitted
     resubmitted_from_id = db.Column(db.Integer)
 
+    _cached_status = None
+    _cached_status_set = None
+
     @property
     def user_name(self):
         return self.user.name
@@ -1020,6 +1023,12 @@ class Build(db.Model, helpers.Serializer):
 
     @property
     def status(self):
+        if getattr(self, '_cached_status_set') is None:
+            self._cached_status = self._status()
+            self._cached_status_set = True
+        return self._cached_status
+
+    def _status(self):
         """
         Return build status.
         """

commit fe8ec942f4b0e2d0b938814541f2fca5f6d77d78

frontend: fix testsuite broken by a34ad40c260e8e61
I'm not 100% sure now that the cached value wouldn't bite us in future;
such problems would be hard to debug.

frontend/coprs_frontend/coprs/models.py
@@ -1023,12 +1023,6 @@ class Build(db.Model, helpers.Serializer):
 
     @property
     def status(self):
-        if getattr(self, '_cached_status_set') is None:
-            self._cached_status = self._status()
-            self._cached_status_set = True
-        return self._cached_status
-
-    def _status(self):
         """
         Return build status.
         """

frontend/coprs_frontend/tests/test_views/test_backend_ns/test_backend_general.py
@@ -317,7 +317,9 @@ class TestUpdateActions(CoprsTestCase):
   ]
 }"""
 
-    def test_update_one_action(self, f_users, f_coprs, f_actions, f_db):
+    @mock.patch('coprs.logic.actions_logic.time.time')
+    def test_update_one_action(self, mc_time, f_users, f_coprs, f_actions, f_db):
+        mc_time.return_value = 1390866440
         r = self.tc.post("/backend/update/",
                          content_type="application/json",
                          headers=self.auth_header,