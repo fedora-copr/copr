fedora-copr/copr

commit 48c101ea5400f76cbe68ccece2f117b06cd3316d

backend: more resilient redis logging
Make sure that log-reader daemon never misses any log entries published
to redis, at least when any log-writer was able to write it correctly.
If writer accidentally fails to publish the log entry, the process
writes at least an error to stderr - so 'systemctl status
copr-backend-*' should uncover the problem.

The previous PUB/SUB was not reliable by design (sender didn't check
how many receivers are listening, and reader had no idea some messages
were lost).  For more info about PUB/SUB reliability and alternatives,
see the redislabs docs [1, 2].

[1] https://redislabs.com/ebook/part-2-core-concepts/chapter-3-commands-in-redis/3-6-publishsubscribe/
[2] https://redislabs.com/ebook/part-2-core-concepts/chapter-6-application-components-in-redis/6-5-pull-messaging/6-5-1-single-recipient-publishsubscribe-replacement/

Relates: #987
Merges: #1164

backend/backend/daemons/log.py
@@ -71,9 +71,7 @@ class RedisLogHandler(object):
         setproctitle("RedisLogHandler")
 
         rc = helpers.get_redis_connection(self.opts)
-        channel = rc.pubsub(ignore_subscribe_messages=True)
-        channel.subscribe(constants.LOG_PUB_SUB)
-
-        for raw in channel.listen():
-            if raw is not None and raw.get("type") == "message" and "data" in raw:
-                self.handle_msg(raw)
+        while True:
+            # indefinitely wait for next entry
+            (_, raw_message) = rc.blpop([constants.LOG_REDIS_FIFO])
+            self.handle_msg(raw_message)

backend/backend/constants.py
@@ -35,7 +35,7 @@ class BuildStatus(object):
         raise AttributeError("no such status id: {0} ".format(number))
 
 
-LOG_PUB_SUB = "copr:backend:log:pubsub::"
+LOG_REDIS_FIFO = "copr:backend:log:fifo::"
 
 default_log_format = Formatter(
     '[%(asctime)s][%(levelname)6s][PID:%(process)d][%(name)10s][%(filename)s:%(funcName)s:%(lineno)d] %(message)s')

backend/backend/helpers.py
@@ -420,7 +420,7 @@ class RedisPublishHandler(logging.Handler):
         record.args = ()
 
         try:
-            self.rc.publish(constants.LOG_PUB_SUB, json.dumps(record.__dict__))
+            self.rc.rpush(constants.LOG_REDIS_FIFO, json.dumps(record.__dict__))
         # pylint: disable=W0703
         except Exception as error:
             _, _, ex_tb = sys.exc_info()

backend/tests/test_helpers.py
@@ -7,7 +7,7 @@ import logging
 from backend.exceptions import BuilderError
 from backend.helpers import get_redis_logger, get_chroot_arch, \
         format_filename, get_redis_connection
-from backend.constants import LOG_PUB_SUB
+from backend.constants import LOG_REDIS_FIFO
 
 """
 SOME TESTS REQUIRES RUNNING REDIS
@@ -24,9 +24,9 @@ class TestHelpers(object):
             redis_port=7777,
         )
 
-        rc = get_redis_connection(self.opts)
-        self.channel = rc.pubsub(ignore_subscribe_messages=True)
-        self.channel.subscribe(LOG_PUB_SUB)
+        self.rc = get_redis_connection(self.opts)
+        # remove leftovers from previous tests
+        self.rc.delete(LOG_REDIS_FIFO)
 
     def teardown_method(self, method):
         pass
@@ -38,14 +38,11 @@ class TestHelpers(object):
         except Exception as err:
             log.exception("error occurred: {}".format(err))
 
-        # read only one message
-        for raw in self.channel.listen():
-            assert raw.get("type") == "message"
-            data = json.loads(raw['data'])
-            assert data.get("who") == "test"
-            assert data.get("levelno") == logging.ERROR
-            assert 'backend.exceptions.BuilderError: foobar\n' in data['msg']
-            break
+        (_, raw_message) = self.rc.blpop([LOG_REDIS_FIFO])
+        data = json.loads(raw_message)
+        assert data.get("who") == "test"
+        assert data.get("levelno") == logging.ERROR
+        assert 'backend.exceptions.BuilderError: foobar\n' in data['msg']
 
     def test_get_chroot_arch(self):
         assert get_chroot_arch("fedora-26-x86_64") == "x86_64"

commit fa0dfc9cacf0085895e7c318c4612bd06e39fa54

backend: RedisLogHandler bugfix
Doing `event = json.loads(raw["data"])` wasn't correct since the
`redis.lrpop()` actually returns directly the data (compared to
`redis.sub()` which contained message structures.

This fixes bug caused by 48c101ea5400f76cbe68ccece2f117b06cd3316d.

backend/backend/daemons/log.py
@@ -50,10 +50,9 @@ class RedisLogHandler(object):
             logger.addHandler(handler)
             self.loggers[component] = logger
 
-    def handle_msg(self, raw):
+    def handle_msg(self, json_event):
         try:
-            event = json.loads(raw["data"])
-
+            event = json.loads(json_event)
             who = event.get('who', None)
             if not who:
                 raise Exception("No LogRecord.who field, raw: {}".format(event))
@@ -73,6 +72,7 @@ class RedisLogHandler(object):
 
         rc = helpers.get_redis_connection(self.opts)
         while True:
-            # indefinitely wait for next entry
-            (_, raw_message) = rc.blpop([constants.LOG_REDIS_FIFO])
-            self.handle_msg(raw_message)
+            # indefinitely wait for the next entry, note that blpop returns
+            # tuple (FIFO_NAME, ELEMENT)
+            (_, json_event) = rc.blpop([constants.LOG_REDIS_FIFO])
+            self.handle_msg(json_event)