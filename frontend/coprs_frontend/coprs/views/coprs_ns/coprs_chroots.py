29273fed03c6c1eba91f808f6ecba92544365d19
Pavel Raiskup <praiskup@redhat.com>
2020-10-02 16:08:54 +0200

    frontend: fix-up the CoprChroot form rendering
    Fixes: #1522

--- frontend/coprs_frontend/coprs/views/coprs_ns/coprs_chroots.py
+++ frontend/coprs_frontend/coprs/views/coprs_ns/coprs_chroots.py
@@ -19,28 +19,28 @@ from coprs.views.coprs_ns import coprs_ns
 @login_required
 @req_with_copr
 def chroot_edit(copr, chrootname):
-    return render_chroot_edit(copr, chrootname)
-
-
-def render_chroot_edit(copr, chroot_name):
-    chroot = ComplexLogic.get_copr_chroot_safe(copr, chroot_name)
-
-    # todo: get COPR_chroot, not mock chroot, WTF?!
-    # form = forms.ChrootForm(buildroot_pkgs=copr.buildroot_pkgs(chroot))
-
+    """ Route for editing CoprChroot's """
+    chroot = ComplexLogic.get_copr_chroot_safe(copr, chrootname)
     form = forms.ChrootForm(buildroot_pkgs=chroot.buildroot_pkgs, repos=chroot.repos,
                             module_toggle=chroot.module_toggle, with_opts=chroot.with_opts,
                             without_opts=chroot.without_opts,
                             bootstrap=chroot.bootstrap,
                             bootstrap_image=chroot.bootstrap_image)
-    # FIXME - test if chroot belongs to copr
+    return render_chroot_edit(form, copr, chroot)
+
+
+def render_chroot_edit(form, copr, chroot):
+    """
+    Using a pre-filled form, copr and chroot instances, render the
+    edit_chroot.html template.
+    """
+
     if flask.g.user.can_build_in(copr):
         return render_template("coprs/detail/edit_chroot.html",
                                form=form, copr=copr, chroot=chroot)
-    else:
-        raise AccessRestricted(
-            "You are not allowed to modify chroots in project {0}."
-            .format(copr.name))
+    raise AccessRestricted(
+        "You are not allowed to modify chroots in project {0}."
+        .format(copr.name))
 
 
 @coprs_ns.route("/<username>/<coprname>/update_chroot/<chrootname>/", methods=["POST"])
@@ -57,40 +57,39 @@ def chroot_update(copr, chrootname):
             "You are not allowed to modify chroots in project {0}."
             .format(copr.name))
 
-    if form.validate_on_submit():
-        if "submit" in flask.request.form:
-            action = flask.request.form["submit"]
-            if action == "update":
-                comps_name = comps_xml = None
+    if not form.validate_on_submit():
+        flask.flash(form.errors, "error")  # pylint: disable=no-member
+        return render_chroot_edit(form, copr, chroot)
 
-                if form.comps.has_file():
-                    comps_xml = form.comps.data.stream.read()
-                    comps_name = form.comps.data.filename
+    if "submit" in flask.request.form:
+        action = flask.request.form["submit"]
+        if action == "update":
+            comps_name = comps_xml = None
 
-                coprs_logic.CoprChrootsLogic.update_chroot(
-                    flask.g.user, chroot,
-                    form.buildroot_pkgs.data,
-                    form.repos.data,
-                    comps=comps_xml, comps_name=comps_name,
-                    with_opts=form.with_opts.data, without_opts=form.without_opts.data,
-                    module_toggle=form.module_toggle.data,
-                    bootstrap=form.bootstrap.data,
-                    bootstrap_image=form.bootstrap_image.data,
-                )
+            if form.comps.has_file():
+                comps_xml = form.comps.data.stream.read()
+                comps_name = form.comps.data.filename
 
-            elif action == "delete_comps":
-                CoprChrootsLogic.remove_comps(flask.g.user, chroot)
+            coprs_logic.CoprChrootsLogic.update_chroot(
+                flask.g.user, chroot,
+                form.buildroot_pkgs.data,
+                form.repos.data,
+                comps=comps_xml, comps_name=comps_name,
+                with_opts=form.with_opts.data, without_opts=form.without_opts.data,
+                module_toggle=form.module_toggle.data,
+                bootstrap=form.bootstrap.data,
+                bootstrap_image=form.bootstrap_image.data,
+            )
 
-            flask.flash(
-                "Buildroot {0} in project {1} has been updated successfully.".format(
-                    chroot_name, copr.name), 'success')
+        elif action == "delete_comps":
+            CoprChrootsLogic.remove_comps(flask.g.user, chroot)
 
-            db.session.commit()
-        return flask.redirect(url_for_copr_edit(copr))
+        flask.flash(
+            "Buildroot {0} in project {1} has been updated successfully.".format(
+                chroot_name, copr.name), 'success')
 
-    else:
-        flask.flash(form.errors, "error")
-        return render_chroot_edit(copr, chroot_name)
+        db.session.commit()
+    return flask.redirect(url_for_copr_edit(copr))
 
 
 @coprs_ns.route("/<username>/<coprname>/chroot/<chrootname>/comps/")
