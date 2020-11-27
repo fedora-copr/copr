a797942461496093a613570d35149651629217b5
Jakub Kadlcik <frostyx@email.cz>
2020-11-27 02:27:14 +0100

    doc: display copr logo in the panel
    I needed to add a new logo with transparent background because our
    Fedora blue color doesn't match the RTD theme blue.
    
    To resize the logo in panel, change the canvas ratio of the
    image. Basically the wider the (transparent) background is, while
    keeping the same height, the smaller it shows in the panel.

--- doc/conf.py
+++ doc/conf.py
@@ -119,7 +119,9 @@ html_css_files = [
 # Theme options are theme-specific and customize the look and feel of a theme
 # further. For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "logo_only": True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -133,7 +135,7 @@ html_css_files = [
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "_static/copr-logo-transparent.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs. This file should be a Windows icon file (.ico) being 16x16 or 32x32
