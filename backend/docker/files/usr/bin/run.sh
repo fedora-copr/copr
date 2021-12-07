0fae34c6b1ae2619ff62173bd209be7a98dc5965
Jakub Kadlcik <frostyx@email.cz>
2021-12-07 10:09:39 +0100

    docker: remove unused backend/docker/ setup
    IIRC this is a leftover Docker setup from a long time ago when we had
    our development environment running in Vagrant with the exception of
    backend, which was dockerized. AFAIK nobody uses this anymore.

--- backend/docker/files/usr/bin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LANG=en_US.UTF-8
-
-echo '127.0.0.1 keygen' >> /etc/hosts
-echo 4096 > /proc/sys/net/core/somaxconn
-/usr/bin/supervisord -c /etc/supervisord.conf
-rngd -r /dev/urandom # using /dev/urandom is a hack just for devel, /dev/hwrandom or /dev/hwrng should be used in production
-/bin/bash
