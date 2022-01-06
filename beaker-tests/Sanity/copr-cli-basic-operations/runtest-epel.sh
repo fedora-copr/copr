c46e258e8acd1ccab97cda820f9154cece8d5fbd
Pavel Raiskup <praiskup@redhat.com>
2022-01-06 11:39:04 +0100

    beaker: test epel-9 and epel-8, too

--- beaker-tests/Sanity/copr-cli-basic-operations/runtest-epel.sh
+++ beaker-tests/Sanity/copr-cli-basic-operations/runtest-epel.sh
@@ -15,7 +15,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest
-        rlRun "copr-cli create ${NAME_PREFIX}TestEpel --chroot epel-6-x86_64 --chroot epel-7-x86_64" 0
+        rlRun "copr-cli create ${NAME_PREFIX}TestEpel --chroot epel-6-x86_64 --chroot epel-7-x86_64 --chroot epel-8-x86_64 --chroot epel-9-x86_64" 0
         rlRun "copr-cli build ${NAME_PREFIX}TestEpel $HELLO" 0
     rlPhaseEnd
 
