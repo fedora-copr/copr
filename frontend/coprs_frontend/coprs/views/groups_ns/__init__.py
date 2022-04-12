36ddb5ad574ae48a1f5410872a2afbf581dd9c4d
Pavel Raiskup <praiskup@redhat.com>
2022-04-12 17:32:04 +0200

    frontend: no need to special-case @group_ns
    The normal @app.errorhandler should take care of this.

--- frontend/coprs_frontend/coprs/views/groups_ns/__init__.py
+++ frontend/coprs_frontend/coprs/views/groups_ns/__init__.py
@@ -2,17 +2,4 @@
 
 import flask
 
-from coprs.views.misc import page_not_found, access_restricted
-from coprs.exceptions import ObjectNotFound, AccessRestricted
-
 groups_ns = flask.Blueprint("groups_ns", __name__, url_prefix="/groups")
-
-
-@groups_ns.errorhandler(ObjectNotFound)
-def handle_404(error):
-    return page_not_found(error.message)
-
-
-@groups_ns.errorhandler(AccessRestricted)
-def handle_403(error):
-    return access_restricted(error.message)
