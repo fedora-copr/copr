fedora-copr/copr

commit a5fe445ee47dee54939d7e06d73cf20b973246f1

frontend: add BuildChroot(s) to Build ASAP if package is known
When the build Package is known, it doesn't make any sense to delay the
assignment of related BuildChroots till the source build is finished.
Nothing can affect the set of BuildChroots, only the chroot denylist
(already known) and potentially some project configuration change (we
can afford this).

This brings a bit more convenience to the monitor web-UI/api routes'
users; users can observe the progress much sooner there (waiting state),
and what's ore important - source build failures are visible there.
Without this change, BuildChroot allocation is delayed to post-source
build and there's a risk of source build failure causing that
BuildChroots are not allocated at all (monitor can not show these
Builds at all).

This patch is trying to centralize the logic for BuildChroot allocation
onto one place.

frontend/coprs_frontend/coprs/logic/builds_logic.py
@@ -690,14 +690,51 @@ class BuildsLogic(object):
         return batch
 
     @classmethod
+    def assign_buildchroots(cls, build, chroot_names, status=None,
+                            git_hashes=None, **kwargs):
+        """
+        For each chroot_name allocate a new BuildChroot instance and assign it
+        to the build.
+        """
+        something_added = False
+        for chroot in chroot_names:
+            something_added = True
+            additional_args = {}
+            if git_hashes:
+                additional_args["git_hash"] = git_hashes.get(chroot)
+            if status is None:
+                status = StatusEnum("waiting")
+            buildchroot = BuildChrootsLogic.new(
+                build=build,
+                mock_chroot=chroot,
+                status=status,
+                **kwargs,
+                **additional_args,
+            )
+            db.session.add(buildchroot)
+        return something_added
+
+    @classmethod
+    def assign_buildchroots_from_package(cls, build, **kwargs):
+        """
+        If build has already assigned build.package, create and assign new set
+        of BuildChroot instances according to 'build.package.chroots'.  This
+        e.g. respects the Copr.chroot_denylist config.
+        """
+        if not build.package:
+            return False
+        return cls.assign_buildchroots(
+            build,
+            build.package.chroots,
+            **kwargs,
+        )
+
+    @classmethod
     def add(cls, user, pkgs, copr, source_type=None, source_json=None,
             repos=None, chroots=None, timeout=None, enable_net=True,
             git_hashes=None, skip_import=False, background=False, batch=None,
             srpm_url=None, copr_dirname=None, bootstrap=None, isolation=None,
             package=None, after_build_id=None, with_build_id=None):
-
-        if chroots is None:
-            chroots = []
 
         coprs_logic.CoprsLogic.raise_if_unfinished_blocking_action(
             copr, "Can't build while there is an operation in progress: {action}")
@@ -755,21 +792,18 @@ class BuildsLogic(object):
         if timeout:
             build.timeout = timeout or app.config["DEFAULT_BUILD_TIMEOUT"]
 
+        if not chroots and package:
+            chroots = package.chroots
+        if not chroots:
+            chroots = []
+
         db.session.add(build)
-
-        for chroot in chroots:
-            # Chroots were explicitly set per-build.
-            git_hash = None
-            if git_hashes:
-                git_hash = git_hashes.get(chroot.name)
-            buildchroot = BuildChrootsLogic.new(
-                build=build,
-                status=chroot_status,
-                mock_chroot=chroot,
-                git_hash=git_hash,
-            )
-            db.session.add(buildchroot)
-
+        cls.assign_buildchroots(
+            build,
+            chroots,
+            git_hashes=git_hashes,
+            status=chroot_status,
+        )
         return build
 
     @classmethod
@@ -814,17 +848,7 @@ class BuildsLogic(object):
             submitted_by=submitted_by,
         )
         db.session.add(build)
-
-        status = StatusEnum("waiting")
-        for chroot in package.chroots:
-            buildchroot = BuildChrootsLogic.new(
-                build=build,
-                status=status,
-                mock_chroot=chroot,
-                git_hash=None
-            )
-            db.session.add(buildchroot)
-
+        cls.assign_buildchroots_from_package(build)
         cls.process_update_callback(build)
         return build
 
@@ -918,19 +942,10 @@ class BuildsLogic(object):
                 new_status = StatusEnum("importing")
                 chroot_status=StatusEnum("waiting")
                 if not build.build_chroots:
-                    # create the BuildChroots from Package setting, if not
-                    # already set explicitly for concrete build
-                    added = False
-                    for chroot in build.package.chroots:
-                        added = True
-                        buildchroot = BuildChrootsLogic.new(
-                            build=build,
-                            status=chroot_status,
-                            mock_chroot=chroot,
-                            git_hash=None,
-                        )
-                        db.session.add(buildchroot)
-                    if not added:
+                    # When BuildChroots are not yet allocated, allocate them now
+                    # from the assigned Package setting.
+                    if not cls.assign_buildchroots_from_package(
+                            build, status=chroot_status, git_hash=None):
                         new_status = StatusEnum("failed")
                 else:
                     for buildchroot in build.build_chroots:

frontend/coprs_frontend/tests/test_apiv3/test_monitor.py
@@ -135,9 +135,7 @@ class TestAPIv3Monitor(CoprsTestCase):
             "test",
             ["fedora-rawhide-i386", "fedora-18-x86_64"])
         self.web_ui.create_distgit_package("test", "tar")
-        self.api3.rebuild_package("test", "tar", build_options={
-            "chroots": ["fedora-rawhide-i386"],
-        })
+        self.api3.rebuild_package("test", "tar")
 
         def _fixup_result(result_dict, update=None):
             for package in result_dict["packages"]:
@@ -159,6 +157,13 @@ class TestAPIv3Monitor(CoprsTestCase):
                 "name": "tar",
                 'chroots': {
                     'fedora-rawhide-i386': {
+                        'build_id': 1,
+                        'state': 'waiting',
+                        'status': 9,
+                        # we don't have build log here
+                        'url_build_log': None,
+                    },
+                    'fedora-18-x86_64': {
                         'build_id': 1,
                         'state': 'waiting',
                         'status': 9,

frontend/coprs_frontend/tests/test_apiv3/test_packages.py
@@ -63,13 +63,21 @@ class TestAPIv3Packages(CoprsTestCase):
         build = self.models.Build.query.get(1)
         assert json.loads(build.source_json) == expected_source_dict
         assert build.source_type == BuildSourceEnum(source_type_text)
-        assert build.chroots == []
 
+        def _assert_default_chroots(test_build):
+            # We assign Package to Build as soon as possible, and at the same
+            # time we allocate BuildChroots.
+            assert {bch.name for bch in test_build.chroots} == {
+                "fedora-17-x86_64",
+                "fedora-17-i386",
+            }
+
+        _assert_default_chroots(build)
         rebuild_data["chroots"] = chroots
         self.post_api3_with_auth(endpoint, rebuild_data, user)
         build = self.models.Build.query.get(2)
         assert json.loads(build.source_json) == expected_source_dict
         if "fedora-18-x86_64" in chroots or chroots == []:
-            assert build.chroots == []
+            _assert_default_chroots(build)
         else:
             assert [mch.name for mch in build.chroots] == ["fedora-17-i386"]

commit 925caa76cfb1f3f55637608ea3d020554c725625

frontend: builds after resubmit have different built packages
The git hash inheritance was broken in commit a5fe445ee47dee54939d7e06d73cf20b973246f1

Fixes: #2130

frontend/coprs_frontend/coprs/logic/builds_logic.py
@@ -731,7 +731,7 @@ class BuildsLogic(object):
             something_added = True
             additional_args = {}
             if git_hashes:
-                additional_args["git_hash"] = git_hashes.get(chroot)
+                additional_args["git_hash"] = git_hashes.get(chroot.name)
             if status is None:
                 status = StatusEnum("waiting")
             buildchroot = BuildChrootsLogic.new(

frontend/coprs_frontend/tests/test_logic/test_builds_logic.py
@@ -379,6 +379,14 @@ class TestBuildsLogic(CoprsTestCase):
         with pytest.raises(NoResultFound):
             BuildsLogic.get(self.b4.id).one()
 
+    @pytest.mark.usefixtures("f_users", "f_coprs", "f_mock_chroots", "f_builds")
+    def test_resubmit_build_inherit_git_hash(self):
+        orig_git_hash = self.b1.build_chroots[0].git_hash
+        self.b1.source_type = 2  # builds from upload should inherit the git hash
+        new_build = BuildsLogic.create_new_from_other_build(self.u1, self.c1, self.b1)
+        new_git_hash = new_build.build_chroots[0].git_hash
+        assert orig_git_hash == new_git_hash
+
     def test_mark_as_failed(self, f_users, f_coprs, f_mock_chroots, f_builds):
         self.b1.source_status = StatusEnum("succeeded")
         self.db.session.commit()