41bfa6d14c0060a6b9bab0be8ddd6caccedc57dc
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2023-04-24 17:13:28 +0200

    dist-git: refresh pyrpkg.Commands after import_srpm
    in case of importing, the content of directory in `reponame`
    changes. To update the state of `Commands` class isn't an easy
    process - look how much logic pyrpkg.cli.cliClient.load_cmd has
    and the logic is written for context in cliClient, not for Commands
    which we are using. Considering this wouldn't be an easy task for
    pyrpkg to implement, we can refresh the Commands class after every
    import - even if it is not elegant

--- dist-git/tests/test_crazy_merging.py
+++ dist-git/tests/test_crazy_merging.py
@@ -189,7 +189,6 @@ class TestMerging(object):
     def setup_method(self, method):
         srpm_cache = {}
 
-    @pytest.mark.skip(reason="Remove the skip once python-rpkg is repared (correct version is 1.65)")
     def test_merged_everything(self, initial_commit_everywhere, mc_setup_git_repo):
         branches, opts, v1_hash = initial_commit_everywhere
         origin, all_branches, middle_branches, border_branches = branches
@@ -209,7 +208,6 @@ class TestMerging(object):
         assert v3_hash != v1_hash
         assert v3_hash != v2_hash
 
-    @pytest.mark.skip(reason="Remove the skip once python-rpkg is repared (correct version is 1.65)")
     def test_diverge_middle_branches(self, initial_commit_everywhere, mc_setup_git_repo):
         branches, opts, v1_hash = initial_commit_everywhere
         origin, all_branches, middle_branches, border_branches = branches
