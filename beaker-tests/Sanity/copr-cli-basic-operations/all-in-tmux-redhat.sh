02ed5f5df08c30c27b3b2c6fe99b116839c78a27
Pavel Raiskup <praiskup@redhat.com>
2023-06-16 17:48:07 +0200

    beaker: pre-create the tested DNF Copr config

--- beaker-tests/Sanity/copr-cli-basic-operations/all-in-tmux-redhat.sh
+++ beaker-tests/Sanity/copr-cli-basic-operations/all-in-tmux-redhat.sh
@@ -3,7 +3,8 @@
 our_dir=$(readlink -f "$(dirname "$0")")
 
 OWNER=$(copr-cli whoami)
-FRONTEND_URL=https://dev-copr.devel.redhat.com
+FRONTEND_HOST=dev-copr.devel.redhat.com
+FRONTEND_URL=https://$FRONTEND_HOST
 BACKEND_URL=https://dev-coprbe.devel.redhat.com
 DISTGIT_URL=https://dev-copr-dist-git.devel.redhat.com
 
@@ -14,6 +15,14 @@ if ! rpm -q rhcopr &>/dev/null; then
     dnf install --nogpgcheck -y --repofrompath="rhcopr,$repo" rhcopr
     cp /etc/rhcopr/rh.crt /etc/pki/ca-trust/source/anchors/rh.crt
     update-ca-trust
+
+    cat > /etc/dnf/plugins/copr.d/tested-copr.conf <<EOF
+[tested-copr]
+hostname = $FRONTEND_HOST
+protocol = https
+port = 443
+EOF
+
 fi
 
 export OWNER FRONTEND_URL BACKEND_URL DISTGIT_URL
