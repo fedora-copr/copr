a43ec21d9caac2e737c3bebdd9b3ca7963a9884a
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2024-03-01 18:08:41 +0100

    frontend: migrate more enpoints to flask-restx

--- frontend/coprs_frontend/coprs/views/apiv3_ns/apiv3_project_chroots.py
+++ frontend/coprs_frontend/coprs/views/apiv3_ns/apiv3_project_chroots.py
@@ -4,8 +4,13 @@
 
 import flask
 from flask_restx import Namespace, Resource
-from coprs.views.misc import api_login_required
-from coprs.views.apiv3_ns import apiv3_ns, api, rename_fields_helper, query_to_parameters
+from coprs.views.misc import restx_api_login_required
+from coprs.views.apiv3_ns import (
+    api,
+    rename_fields_helper,
+    query_to_parameters,
+    deprecated_route_method_type,
+)
 from coprs.views.apiv3_ns.schema.schemas import (
     project_chroot_model,
     project_chroot_build_config_model,
@@ -17,10 +22,9 @@ from coprs import db, forms
 from coprs.logic.coprs_logic import CoprChrootsLogic
 from . import (
     get_copr,
-    file_upload,
-    PUT,
     str_to_list,
     reset_to_defaults,
+    restx_file_upload,
 )
 from .json2form import get_form_compatible_data
 
@@ -105,43 +109,65 @@ class BuildConfig(Resource):
         return to_build_config_dict(chroot)
 
 
-@apiv3_ns.route("/project-chroot/edit/<ownername>/<projectname>/<chrootname>", methods=PUT)
-@file_upload()
-@api_login_required
-def edit_project_chroot(ownername, projectname, chrootname):
-    copr = get_copr(ownername, projectname)
-    data = rename_fields(get_form_compatible_data(preserve=["additional_modules"]))
-    form = forms.ModifyChrootForm(data, meta={'csrf': False})
-    chroot = ComplexLogic.get_copr_chroot(copr, chrootname)
+@apiv3_project_chroots_ns.route("/edit/<ownername>/<projectname>/<chrootname>")
+class EditChroot(Resource):
+    def _common(self, ownername, projectname, chrootname):
+        copr = get_copr(ownername, projectname)
+        data = rename_fields(get_form_compatible_data(preserve=["additional_modules"]))
+        form = forms.ModifyChrootForm(data, meta={'csrf': False})
+        chroot = ComplexLogic.get_copr_chroot(copr, chrootname)
 
-    if not form.validate_on_submit():
-        raise InvalidForm(form)
+        if not form.validate_on_submit():
+            raise InvalidForm(form)
 
-    more_fields = ("See `copr-cli get-chroot {0}' for all the possible "
-                   "attributes".format(chroot.full_name))
-    reset_to_defaults(chroot, form, rename_fields, more_fields)
+        more_fields = ("See `copr-cli get-chroot {0}' for all the possible "
+                       "attributes".format(chroot.full_name))
+        reset_to_defaults(chroot, form, rename_fields, more_fields)
 
-    buildroot_pkgs = repos = module_toggle = comps_xml = comps_name = with_opts = without_opts = None
-    if "buildroot_pkgs" in data:
-        buildroot_pkgs = form.buildroot_pkgs.data
-    if "repos" in data:
-        repos = form.repos.data
-    if "module_toggle" in data:
-        module_toggle = form.module_toggle.data
-    if "with_opts" in data:
-        with_opts = form.with_opts.data
-    if "without_opts" in data:
-        without_opts = form.without_opts.data
-    if form.upload_comps.data:
-        comps_xml = form.upload_comps.data.stream.read()
-        comps_name = form.upload_comps.data.filename
-    if form.delete_comps.data:
-        CoprChrootsLogic.remove_comps(flask.g.user, chroot)
-    CoprChrootsLogic.update_chroot(
-        flask.g.user, chroot, buildroot_pkgs, repos, comps=comps_xml, comps_name=comps_name,
-        with_opts=with_opts, without_opts=without_opts, module_toggle=module_toggle,
-        bootstrap=form.bootstrap.data,
-        bootstrap_image=form.bootstrap_image.data,
-        isolation=form.isolation.data)
-    db.session.commit()
-    return flask.jsonify(to_dict(chroot))
+        buildroot_pkgs = repos = module_toggle = comps_xml = comps_name = with_opts = without_opts = None
+        if "buildroot_pkgs" in data:
+            buildroot_pkgs = form.buildroot_pkgs.data
+        if "repos" in data:
+            repos = form.repos.data
+        if "module_toggle" in data:
+            module_toggle = form.module_toggle.data
+        if "with_opts" in data:
+            with_opts = form.with_opts.data
+        if "without_opts" in data:
+            without_opts = form.without_opts.data
+        if form.upload_comps.data:
+            comps_xml = form.upload_comps.data.stream.read()
+            comps_name = form.upload_comps.data.filename
+        if form.delete_comps.data:
+            CoprChrootsLogic.remove_comps(flask.g.user, chroot)
+        CoprChrootsLogic.update_chroot(
+            flask.g.user, chroot, buildroot_pkgs, repos, comps=comps_xml, comps_name=comps_name,
+            with_opts=with_opts, without_opts=without_opts, module_toggle=module_toggle,
+            bootstrap=form.bootstrap.data,
+            bootstrap_image=form.bootstrap_image.data,
+            isolation=form.isolation.data)
+        db.session.commit()
+        return to_dict(chroot)
+
+    @deprecated_route_method_type(apiv3_project_chroots_ns, "POST", "PUT")
+    @restx_file_upload
+    @restx_api_login_required
+    @apiv3_project_chroots_ns.doc(params=project_chroot_get_params)
+    @apiv3_project_chroots_ns.marshal_with(project_chroot_model)
+    def post(self, ownername, projectname, chrootname):
+        """
+        Edit a project chroot
+        Edit settings for a single project chroot.
+        """
+        return self._common(ownername, projectname, chrootname)
+
+    @restx_file_upload
+    @restx_api_login_required
+    @apiv3_project_chroots_ns.doc(params=project_chroot_get_params)
+    @apiv3_project_chroots_ns.marshal_with(project_chroot_model)
+    def put(self, ownername, projectname, chrootname):
+        """
+        Edit a project chroot
+        Edit settings for a single project chroot.
+        """
+        return self._common(ownername, projectname, chrootname)
