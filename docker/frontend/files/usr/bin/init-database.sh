6f86355326f302f37ae2c203b0b40301f6e2012c
Kimpe Andy <andykimpe@gmail.com>
2024-06-07 12:52:59 +0200

    Update init-database.sh

--- docker/frontend/files/usr/bin/init-database.sh
+++ docker/frontend/files/usr/bin/init-database.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 cd /usr/share/copr/coprs_frontend/ && copr-frontend create-db --alembic alembic.ini
-copr-frontend create-chroot \
-    $(ls /etc/mock/{fedora,epel}-*-{i386,x86_64}.cfg |xargs -I{} -n1 basename {} .cfg)
+copr-frontend create-chroot epel-next-9-x86_64-rpmfusion_nonfree
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
+copr-frontend create-chroot 
