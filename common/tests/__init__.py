dd06d17a226be982eb95aeff25783f7548abc191
Miroslav Suchý <msuchy@redhat.com>
2024-07-31 17:14:39 +0200

    common: drop support for rhel7 and rhel6

--- common/tests/__init__.py
+++ common/tests/__init__.py
@@ -1,8 +1,2 @@
-import six
-
-if six.PY3:
-    from unittest import mock
-    from unittest.mock import MagicMock
-else:
-    import mock
-    from mock import MagicMock
+from unittest import mock
+from unittest.mock import MagicMock
