d51812424139a9b96f231862e0791e789f5d4db9
Pavel Raiskup <praiskup@redhat.com>
2025-12-04 20:17:25 +0100

    testing-farm: install pre-release packages with Ansible
    It's better to keep the test script focused on testing, not machine
    preparation.  I needed to revamp some old scripting related to GitHub
    automation so it is usable for Testing Farm, too.

--- testing-farm/tests/sanity/test.sh
+++ testing-farm/tests/sanity/test.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -eux
 
 cd "$(git rev-parse --show-toplevel)"
-
-# Note:
-# https://gitlab.com/testing-farm/tests/-/issues/2
-packages=$(PYTHON_PKG_SUFFIX=3 ./releng/detect-changed-packages "$PACKIT_TARGET_SHA")
-./releng/install-copr-packages "@copr/copr-pull-requests:pr:$PACKIT_PR_ID" "$PACKIT_COMMIT_SHA" $packages
+rpm -qa | grep copr
