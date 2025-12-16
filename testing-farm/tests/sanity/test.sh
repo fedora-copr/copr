febb96363265bfee545fe0a09ff70482cb4eea5b
Pavel Raiskup <praiskup@redhat.com>
2025-12-16 15:08:14 +0100

    behave/testing-farm: run tests in Testing Farm
    Add a new test script, all-on-single-host.sh, used for single-host
    testing in Testing Farm.  This script serves as a new test runtime that
    executes (all) tests asynchronously, collects results as they complete,
    and presents the output to the user.
    
    Implemented a workaround for Beakerlib's failure to propagate non-zero
    exit statuses to executing scripts; this ensures that all scripts
    correctly exit with an error code upon failure.
    
    Implemented workarounds for Ansible's ansible_host variable to handle
    cases where it unexpectedly includes a root@ prefix.
    
    Pre-generate some useful defaults in Frontend's database (e.g., dist-git
    instance references, CoprChroot instances, etc.).
    
    Pre-configure DNF in the tested environment, so we can easily
    `dnf copr enable`.
    
    Configure copr-rpmbuild's main.ini config, so it knows how to build from
    the single-host dist-git.
    
    Closes: #4090

--- testing-farm/tests/sanity/test.sh
+++ testing-farm/tests/sanity/test.sh
@@ -2,3 +2,11 @@
 
 cd "$(git rev-parse --show-toplevel)"
 rpm -qa | grep copr
+
+lscpu || :
+lsmem || :
+swapon || :
+dnf copr list
+cat /etc/yum.repos.d/*.repo
+
+./testing-farm/all-on-single-host.sh
