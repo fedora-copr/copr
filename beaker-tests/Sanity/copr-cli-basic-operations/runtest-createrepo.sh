8ecd5a1d9dae7c1713976a1c62a36960bde9f8f6
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2026-02-12 20:36:47 +0100

    working checkpoint

--- beaker-tests/Sanity/copr-cli-basic-operations/runtest-createrepo.sh
+++ beaker-tests/Sanity/copr-cli-basic-operations/runtest-createrepo.sh
@@ -7,8 +7,6 @@ HERE=$(dirname "$(realpath "$0")")
 source "$HERE/config"
 source "$HERE/helpers"
 
-URL=${FRONTEND_URL#https://}
-
 rlJournalStart
     rlPhaseStartSetup
         setup_checks
@@ -17,7 +15,7 @@ rlJournalStart
     rlPhaseStartTest
         rlRun "copr-cli create ${NAME_PREFIX}Createrepo --chroot $CHROOT"
         # don't specify chroot here, rely on auto-detection
-        rlRun "dnf -y copr enable ${URL}/${NAME_PREFIX}Createrepo"
+        rlRun "dnf -y copr enable $DNF_COPR_ID/${NAME_PREFIX}Createrepo"
         rlRun "dnf --disablerepo='*' \
             --enablerepo='copr:${FRONTEND_PUBLIC_HOST}:$(repo_owner):${NAME_VAR}Createrepo' \
             list available 2>&1 | grep 'Failed to synchronize'" 1
@@ -27,7 +25,7 @@ rlJournalStart
         echo "wait 2+ minutes to invalidate cache"
         echo "https://github.com/fedora-copr/copr/blob/526473b43b5e0c1f84f7db624f349a50a8e2b7d9/frontend/coprs_frontend/coprs/views/apiv3_ns/apiv3_rpmrepo.py#L37"
         sleep 125
-        rlRun "dnf -y copr enable ${URL}/${NAME_PREFIX}Createrepo fedora-rawhide-x86_64"
+        rlRun "dnf -y copr enable $DNF_COPR_ID/${NAME_PREFIX}Createrepo fedora-rawhide-x86_64"
         rlRun "dnf --disablerepo='*' \
             --enablerepo='copr:${FRONTEND_PUBLIC_HOST}:$(repo_owner):${NAME_VAR}Createrepo' \
             list available 2>&1 | grep 'Failed to synchronize'" 1
@@ -35,7 +33,7 @@ rlJournalStart
 
     rlPhaseStartCleanup
         cleanProject "${NAME_PREFIX}Createrepo"
-        rlRun "dnf -y copr remove ${URL}/${NAME_PREFIX}Createrepo"
+        rlRun "dnf -y copr remove $DNF_COPR_ID/${NAME_PREFIX}Createrepo"
     rlPhaseEnd
 rlJournalPrintText
 rlJournalEnd
