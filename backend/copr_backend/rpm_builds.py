df2053838253a83a0c87cb7b7b605dffc350b2b7
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2026-06-02 10:56:35 +0200

    Revert "backend: block all actions and builds for @copr/PyPI and @copr/PyPI3"
    This reverts PR #4327 (commits 5fff8c3af and 8addefab8).
    Requested by @FrostyX.
    
    (cherry picked from commit c36fd49aec9c9b00c85f5f05b5c0714ac1589979)

--- backend/copr_backend/rpm_builds.py
+++ backend/copr_backend/rpm_builds.py
@@ -143,12 +143,6 @@ class BlockedOwnersLimit(PredicateWorkerLimit):
     """
     def __init__(self, blocked_owners):
         def predicate(x):
-            # We don't have the `name` or `full_name` available, so the next
-            # best thing is checking sandbox
-            pypi = ("@copr/PyPI--", "@copr/PyPI3--")
-            sandbox = getattr(x, "sandbox", None)
-            if sandbox and sandbox.startswith(pypi):
-                return True
             return x.owner in blocked_owners
         super().__init__(predicate, limit=0, name="blocked_users")
 
